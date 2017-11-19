27c9ad3a36cee1dfd5953bc3719268cd4cc499ed
Max Lynch <max@drifty.com>
2017-11-19 16:38:41 -0600

    isVirtual

--- packages/example/src/app/app.component.ts
+++ packages/example/src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
+import { HomePage } from '../pages/home/home';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = TabsPage;
+  rootPage = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
