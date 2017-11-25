91a34ce22ad5dc71f8918f1508ad687cb5a545fc
Max Lynch <max@drifty.com>
2017-11-24 19:30:41 -0600

    Properly check to see if Class conforms to protocol

--- packages/ios/Avocado/Avocado/Avocado.h
+++ packages/ios/Avocado/Avocado/Avocado.h
@@ -8,6 +8,7 @@ FOUNDATION_EXPORT const unsigned char AvocadoVersionString[];
 
 extern NSArray<Class> *AvocadoGetModuleClasses(void);
 
+#import "Plugins.h"
 #import "CorePlugins.h"
 
 // In this header, you should import all the public headers of your framework using statements like #import <Avocado/PublicHeader.h>
