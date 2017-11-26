7ce507b9527e512054349c7ed59aa131f6ece7c6
Max Lynch <max@drifty.com>
2017-11-25 21:13:19 -0600

    Camera check usage descriptions

--- packages/ios/Avocado/Avocado/Modules/Camera/Camera.swift
+++ packages/ios/Avocado/Avocado/Modules/Camera/Camera.swift
@@ -12,10 +12,35 @@ public class Camera : Plugin {
       call.error("Camera not available while running in Simulator")
     }
     
+    if let missingUsageDescription = checkUsageDescriptions() {
+      avocado.modulePrint(self, missingUsageDescription)
+      call.error(missingUsageDescription)
+      avocado.alert("Camera Error", "Missing required usage description. See console for more information")
+      return
+    }
+    
     let viewController = UIStoryboard.init(name: "Camera", bundle: Bundle(for: type(of: self))).instantiateViewController(withIdentifier: "CameraViewController")
     
     self.avocado.viewController.present(viewController, animated: true) {
     }
+  }
+  
+  
+  /**
+   * Make sure the developer provided proper usage descriptions
+   * per apple's terms.
+   */
+  func checkUsageDescriptions() -> String? {
+    if let dict = Bundle.main.infoDictionary {
+      let hasPhotoLibraryUsage = dict["NSPhotoLibraryAddUsageDescription"] != nil
+      if !hasPhotoLibraryUsage {
+        let docLink = DocLinks.NSPhotoLibraryAddUsageDescription
+        return "You are missing NSPhotoLibraryAddUsageDescription in your Info.plist file." +
+        " Camera will not function without it. Learn more: \(docLink.rawValue)"
+      }
+    }
+    
+    return nil
   }
 }
 
--- packages/ios/Avocado/Avocado/Modules/Camera/CameraViewController.swift
+++ packages/ios/Avocado/Avocado/Modules/Camera/CameraViewController.swift
@@ -34,22 +34,65 @@ public class CameraViewController : UIViewController, AVCapturePhotoCaptureDeleg
   var stillImageOutput: AVCapturePhotoOutput?
   var videoPreviewLayer: AVCaptureVideoPreviewLayer?
   
-  
   @IBOutlet weak var previewView: UIView!
   @IBOutlet weak var captureButton: UIButton!
   @IBOutlet weak var lastImageView: UIImageView!
   
   @IBAction func takePicture(_ sender: Any) {
-    print("Taking picture")
-    let settings = AVCapturePhotoSettings()
-    
+    let settings = AVCapturePhotoSettings.init(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
+    settings.isAutoStillImageStabilizationEnabled = true
+    settings.flashMode = .off
     stillImageOutput?.capturePhoto(with: settings, delegate: self)
   }
 
   public func photoOutput(_ output: AVCapturePhotoOutput,
                             didFinishProcessingPhoto photo: AVCapturePhoto,
                             error: Error?) {
-    print("Captured", output)
+    // Check if there is any error in capturing
+    guard error == nil else {
+      print("Fail to capture photo: \(String(describing: error))")
+      return
+    }
+    
+    // Check if the pixel buffer could be converted to image data
+    guard let imageData = photo.fileDataRepresentation() else {
+      print("Fail to convert pixel buffer")
+      return
+    }
+    
+    // Check if UIImage could be initialized with image data
+    guard let capturedImage = UIImage.init(data: imageData , scale: 1.0) else {
+      print("Fail to convert image data to UIImage")
+      return
+    }
+    
+    // Get original image width/height
+    let imgWidth = capturedImage.size.width
+    let imgHeight = capturedImage.size.height
+    // Get origin of cropped image
+    let imgOrigin = CGPoint(x: (imgWidth - imgHeight)/2, y: (imgHeight - imgHeight)/2)
+    // Get size of cropped iamge
+    let imgSize = CGSize(width: imgHeight, height: imgHeight)
+    
+    // Check if image could be cropped successfully
+    guard let imageRef = capturedImage.cgImage?.cropping(to: CGRect(origin: imgOrigin, size: imgSize)) else {
+      print("Fail to crop image")
+      return
+    }
+    
+    // Convert cropped image ref to UIImage
+    let imageToSave = UIImage(cgImage: imageRef, scale: 1.0, orientation: .down)
+    UIImageWriteToSavedPhotosAlbum(imageToSave, nil, nil, nil)
+    
+    lastImageView.image = imageToSave
+    
+    // Stop video capturing session (Freeze preview)
+    session.stopRunning()
+  }
+  
+  // TODO:
+  func getDepthData(photo: AVCapturePhoto) -> Float32 {
+    return 0
   }
 
   
