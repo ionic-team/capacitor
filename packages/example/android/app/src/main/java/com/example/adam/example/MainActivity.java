70edc426d83e62c23087b27b1161f66da610006a
Adam Bradley <adambradley25@gmail.com>
2017-11-29 13:25:35 -0600

    Android example refactor

--- packages/example/android/app/src/main/java/com/example/adam/example/MainActivity.java
+++ packages/example/android/app/src/main/java/com/example/adam/example/MainActivity.java
@@ -3,8 +3,9 @@ package com.example.adam.example;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
-import com.ionicframework.plugins.device.DevicePlugin;
-import com.ionicframework.avocado.Avocado;
+import com.avocadojs.Avocado;
+import com.avocadojs.plugin.device.DevicePlugin;
+
 
 public class MainActivity extends AppCompatActivity {
 
