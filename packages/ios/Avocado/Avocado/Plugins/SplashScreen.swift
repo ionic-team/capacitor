eb7b15a39b7062eb1cdaeef23a0ae171714f50ba
Max Lynch <max@drifty.com>
2017-11-29 09:48:01 -0800

    Splash screen

--- packages/ios/Avocado/Avocado/Plugins/SplashScreen.swift
+++ packages/ios/Avocado/Avocado/Plugins/SplashScreen.swift
@@ -11,26 +11,24 @@ public class SplashScreen : Plugin {
     buildViews()
   }
   
+  // Show the splash screen
   @objc public func show(_ call: PluginCall) {
     showSplash()
     call.success()
   }
   
+  // Hide the splash screen
   @objc public func hide(_ call: PluginCall) {
     hideSplash()
     call.success()
   }
   
   func buildViews() {
-    guard let launchImage = Bundle.main.object(forInfoDictionaryKey: "UILaunchStoryboardName") as? String else {
-      bridge.modulePrint(self, "No Launch Storyboard found. SplashScreen plugin requires it.")
-      return
-    }
-    
-    print("Building splash view")
+    // Find the image asset named "Splash"
+    // TODO: Find a way to not hard code this?
     image = UIImage.init(named: "Splash")
     
-    // TODO: Handle rotation for screens/ipad
+    // Observe for changes on fram and bounds to handle rotation resizing
     let parentView = self.bridge.viewController.view
     parentView?.addObserver(self, forKeyPath: "frame", options: .new, context: nil)
     parentView?.addObserver(self, forKeyPath: "bounds", options: .new, context: nil)
@@ -38,6 +36,8 @@ public class SplashScreen : Plugin {
     self.updateSplashImageBounds()
   }
   
+  // Update the bounds for the splash image. This will also be called when
+  // the parent view observers fire
   func updateSplashImageBounds() {
     guard let delegate = UIApplication.shared.delegate else {
       bridge.modulePrint(self, "Unable to find root window object for SplashScreen bounds. Please file an issue")
@@ -48,13 +48,13 @@ public class SplashScreen : Plugin {
       bridge.modulePrint(self, "Unable to find root window object for SplashScreen bounds. Please file an issue")
       return
     }
+    
     imageView.image = image
     imageView.frame = CGRect.init(origin: CGPoint.init(x: 0, y: 0), size: window.bounds.size)
     imageView.contentMode = .scaleAspectFill
   }
   
   public override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-    print("Updating splash image bounds")
     updateSplashImageBounds()
   }
   
@@ -62,12 +62,16 @@ public class SplashScreen : Plugin {
     bridge.viewController.view.addSubview(imageView)
     
     bridge.viewController.view.isUserInteractionEnabled = false
+    
+    // TODO: Fade in
   }
   
   func hideSplash() {
     bridge.viewController.view.isUserInteractionEnabled = true
 
     imageView.removeFromSuperview()
+    
+    // TODO: Fade out
   }
 }
 
