f3662cf6477eeb104b870a135d5c4a0559e1ff98
Max Lynch <max@drifty.com>
2017-12-01 15:51:25 -0600

    Refactoring Android

---
+++ packages/android/avocado/src/main/java/com/avocadojs/plugin/Filesystem.java
@@ -0,0 +1,39 @@
+package com.avocadojs.plugin;
+
+import com.avocadojs.Avocado;
+import com.avocadojs.Plugin;
+import com.avocadojs.PluginCall;
+
+public class Filesystem extends Plugin {
+  public Filesystem(Avocado avocado) {
+    super(avocado, "com.avocadojs.plugin.filesystem");
+  }
+
+  public void readFile(PluginCall call) {
+    String level = call.data.optString("level", "log").toLowerCase();
+  }
+
+  public void writeFile(PluginCall call) {
+
+  }
+
+  public void appendFile(PluginCall call) {
+
+  }
+
+  public void mkdir(PluginCall call) {
+
+  }
+
+  public void rmdir(PluginCall call) {
+
+  }
+
+  public void readdir(PluginCall call) {
+
+  }
+
+  public void stat(PluginCall call) {
+
+  }
+}
