d94a3c3dc6abd6e4953585b10662ae066139c769
Julio César <jcesarmobile@gmail.com>
2017-12-01 15:22:40 +0100

    Added other usage descriptions for Camera usage

--- packages/ios/Avocado/Avocado/DocLinks.swift
+++ packages/ios/Avocado/Avocado/DocLinks.swift
@@ -10,5 +10,7 @@ import Foundation
 
 enum DocLinks: String {
   case NSPhotoLibraryAddUsageDescription = "https://developer.apple.com/library/content/documentation/General/Reference/InfoPlistKeyReference/Articles/CocoaKeys.html#//apple_ref/doc/uid/TP40009251-SW73"
+  case NSPhotoLibraryUsageDescription = "https://developer.apple.com/library/content/documentation/General/Reference/InfoPlistKeyReference/Articles/CocoaKeys.html#//apple_ref/doc/uid/TP40009251-SW17"
+  case NSCameraUsageDescription = "https://developer.apple.com/library/content/documentation/General/Reference/InfoPlistKeyReference/Articles/CocoaKeys.html#//apple_ref/doc/uid/TP40009251-SW24"
 }
 
