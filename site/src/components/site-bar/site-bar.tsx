f544e571d4c88d0f85ae019ba2c702e15fa25122
Max Lynch <max@drifty.com>
2017-12-05 13:28:00 -0600

    Content

---
+++ site/src/components/site-bar/site-bar.tsx
@@ -0,0 +1,15 @@
+import { Component } from '@stencil/core';
+
+@Component({
+  tag: 'site-bar',
+  styleUrl: 'site-bar.scss'
+})
+export class SiteBar {
+  render() {
+    return (
+      <div class="site-bar">
+        Avocado is in Private Preview. Read the <stencil-route-link url="/blog">announcement blog</stencil-route-link> for more info on the project
+      </div>
+    );
+  }
+}
