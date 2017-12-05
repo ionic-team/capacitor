dc70dfd7d72947cc28b1afe9fa8e17170830123a
Adam Bradley <adambradley25@gmail.com>
2017-12-05 11:44:11 -0600

    init android template

---
+++ android-template/app/src/main/java/io/avocadojs/android/helloapp/MainActivity.java
@@ -0,0 +1,15 @@
+package io.avocadojs.android.helloapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
