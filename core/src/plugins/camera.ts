6e04ba23bfe017e2a93f3e64783b68d8dba9266b
Max Lynch <max@drifty.com>
2017-12-06 16:50:56 -0600

    Camera API

--- core/src/plugins/camera.ts
+++ core/src/plugins/camera.ts
@@ -1,23 +1,20 @@
 import { NativePlugin, Plugin } from '../plugin';
 
-
 @NativePlugin({
   name: 'Camera',
   id: 'com.avocadojs.plugin.camera'
 })
 export class Camera extends Plugin {
-
   getPhoto(options: CameraOptions): Promise<CameraPhoto> {
     return this.nativePromise('getPhoto', options);
   }
-
 }
 
-
 export interface CameraOptions {
-  quality?: number;
-  allowEditing?: boolean;
+  quality?: number; // default: 100
+  allowEditing?: boolean; // default: false
   resultType: 'base64' | 'uri';
+  saveToGallery?: boolean; // default: true
 }
 
 export interface CameraPhoto {
