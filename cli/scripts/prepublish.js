9fd9b0a28f2b6b878d04a4da08d9546679358e33
Adam Bradley <adambradley25@gmail.com>
2017-12-05 21:30:43 -0600

    cli prepublish asset copy task

--- cli/scripts/prepublish.js
+++ cli/scripts/prepublish.js
@@ -2,17 +2,17 @@ const fs = require('fs-extra');
 const path = require('path');
 
 const MONOREPO_ROOT = path.join(__dirname, '../..');
-const RESOURCES_DST = path.join(__dirname, '../resources');
+const ASSETS_DIST = path.join(__dirname, '../assets');
 
 const ANDROID_TEMPLATE = 'android-template';
 const ANDROID_TEMPLATE_SRC = path.join(MONOREPO_ROOT, ANDROID_TEMPLATE);
-const ANDROID_TEMPLATE_DST = path.join(RESOURCES_DST, ANDROID_TEMPLATE);
+const ANDROID_TEMPLATE_DST = path.join(ASSETS_DIST, ANDROID_TEMPLATE);
 
 const IOS_TEMPLATE = 'ios-template';
 const IOS_TEMPLATE_SRC = path.join(MONOREPO_ROOT, IOS_TEMPLATE);
-const IOS_TEMPLATE_DST = path.join(RESOURCES_DST, IOS_TEMPLATE);
+const IOS_TEMPLATE_DST = path.join(ASSETS_DIST, IOS_TEMPLATE);
 
-fs.emptyDirSync(RESOURCES_DST);
+fs.emptyDirSync(ASSETS_DIST);
 
 fs.copy(ANDROID_TEMPLATE_SRC, ANDROID_TEMPLATE_DST);
 fs.copy(IOS_TEMPLATE_SRC, IOS_TEMPLATE_DST);
