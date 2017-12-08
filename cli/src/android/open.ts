4eff6002bbf03b45a38ff94ae39b0535be7ae236
Adam Bradley <adambradley25@gmail.com>
2017-12-07 22:40:49 -0600

    fix(cli): print android location for open cmd

--- cli/src/android/open.ts
+++ cli/src/android/open.ts
@@ -1,17 +1,7 @@
 import { Config } from '../config';
-import { wait } from '../common';
+import { logInfo } from '../common';
 
 
 export async function openAndroid(config: Config) {
-  config;
-  const androidProject = 'TODO';
-
-  if (androidProject) {
-    const opn = require('opn');
-    await opn(androidProject, { wait: false });
-    await wait(3000);
-
-  } else {
-    throw 'Android project does not exist. Run "avocado create android" to bootstrap a native android project.';
-  }
+  return logInfo(`Android project found in: ${config.android.platformDir}`);
 }
