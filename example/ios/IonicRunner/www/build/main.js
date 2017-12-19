9d6d203b15704e089db25e13fa0beb2e28de50f2
Max Lynch <max@drifty.com>
2017-12-19 17:30:43 -0600

    Thinking through local notifs

--- example/ios/IonicRunner/www/build/main.js
+++ example/ios/IonicRunner/www/build/main.js
@@ -22,15 +22,15 @@ webpackEmptyAsyncContext.id = 109;
 
 var map = {
 	"../pages/accessibility/accessibility.module": [
-		277,
+		278,
 		16
 	],
 	"../pages/admin/admin.module": [
-		278,
+		279,
 		15
 	],
 	"../pages/browser/browser.module": [
-		279,
+		277,
 		14
 	],
 	"../pages/camera/camera.module": [
@@ -42,15 +42,15 @@ var map = {
 		12
 	],
 	"../pages/device/device.module": [
-		282,
+		283,
 		11
 	],
 	"../pages/filesystem/filesystem.module": [
-		283,
+		284,
 		10
 	],
 	"../pages/geolocation/geolocation.module": [
-		284,
+		282,
 		9
 	],
 	"../pages/haptics/haptics.module": [
@@ -58,11 +58,11 @@ var map = {
 		8
 	],
 	"../pages/keyboard/keyboard.module": [
-		286,
+		287,
 		7
 	],
 	"../pages/local-notifications/local-notifications.module": [
-		287,
+		286,
 		6
 	],
 	"../pages/modals/modals.module": [
@@ -362,17 +362,17 @@ AppModule = __decorate([
                 preloadModules: true
             }, {
                 links: [
+                    { loadChildren: '../pages/browser/browser.module#BrowserPageModule', name: 'BrowserPage', segment: 'browser', priority: 'low', defaultHistory: [] },
                     { loadChildren: '../pages/accessibility/accessibility.module#AccessibilityPageModule', name: 'AccessibilityPage', segment: 'accessibility', priority: 'low', defaultHistory: [] },
                     { loadChildren: '../pages/admin/admin.module#AdminPageModule', name: 'AdminPage', segment: 'admin', priority: 'low', defaultHistory: [] },
-                    { loadChildren: '../pages/browser/browser.module#BrowserPageModule', name: 'BrowserPage', segment: 'browser', priority: 'low', defaultHistory: [] },
                     { loadChildren: '../pages/camera/camera.module#CameraPageModule', name: 'CameraPage', segment: 'camera', priority: 'low', defaultHistory: [] },
                     { loadChildren: '../pages/clipboard/clipboard.module#ClipboardPageModule', name: 'ClipboardPage', segment: 'clipboard', priority: 'low', defaultHistory: [] },
+                    { loadChildren: '../pages/geolocation/geolocation.module#GeolocationPageModule', name: 'GeolocationPage', segment: 'geolocation', priority: 'low', defaultHistory: [] },
                     { loadChildren: '../pages/device/device.module#DevicePageModule', name: 'DevicePage', segment: 'device', priority: 'low', defaultHistory: [] },
                     { loadChildren: '../pages/filesystem/filesystem.module#FilesystemPageModule', name: 'FilesystemPage', segment: 'filesystem', priority: 'low', defaultHistory: [] },
-                    { loadChildren: '../pages/geolocation/geolocation.module#GeolocationPageModule', name: 'GeolocationPage', segment: 'geolocation', priority: 'low', defaultHistory: [] },
                     { loadChildren: '../pages/haptics/haptics.module#HapticsPageModule', name: 'HapticsPage', segment: 'haptics', priority: 'low', defaultHistory: [] },
-                    { loadChildren: '../pages/keyboard/keyboard.module#KeyboardPageModule', name: 'KeyboardPage', segment: 'keyboard', priority: 'low', defaultHistory: [] },
                     { loadChildren: '../pages/local-notifications/local-notifications.module#LocalNotificationsPageModule', name: 'LocalNotificationsPage', segment: 'local-notifications', priority: 'low', defaultHistory: [] },
+                    { loadChildren: '../pages/keyboard/keyboard.module#KeyboardPageModule', name: 'KeyboardPage', segment: 'keyboard', priority: 'low', defaultHistory: [] },
                     { loadChildren: '../pages/modals/modals.module#ModalsPageModule', name: 'ModalsPage', segment: 'modals', priority: 'low', defaultHistory: [] },
                     { loadChildren: '../pages/motion/motion.module#MotionPageModule', name: 'MotionPage', segment: 'motion', priority: 'low', defaultHistory: [] },
                     { loadChildren: '../pages/network/network.module#NetworkPageModule', name: 'NetworkPage', segment: 'network', priority: 'low', defaultHistory: [] },
