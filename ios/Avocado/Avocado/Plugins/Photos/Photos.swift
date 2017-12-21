35880fc3aa9e148a3db182a7165b5448b9f68079
Max Lynch <max@drifty.com>
2017-12-21 16:55:51 -0600

    User denied access

--- ios/Avocado/Avocado/Plugins/Photos/Photos.swift
+++ ios/Avocado/Avocado/Plugins/Photos/Photos.swift
@@ -15,22 +15,27 @@ public class Photos : AVCPlugin {
     
     return ret
   }
+  
+  
   @objc func getPhotos(_ call: AVCPluginCall) {
     let quantity = call.getInt("quantity", defaultValue: Photos.DEFAULT_QUANTITY)!
     let after = call.getString("after")
     let types = call.getString("types") ?? Photos.DEFAULT_TYPES
   
-    var fetchResult: PHFetchResult<PHAsset>!
-    let options = PHFetchOptions()
-    options.fetchLimit = quantity
-    options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
-    fetchResult = PHAsset.fetchAssets(with: options)
-    
-    let photos = fetchResultAssetsToJs(fetchResult)
-    
-    call.success([
-      "photos": photos
-    ])
+    checkAuthorization(call) {
+      var fetchResult: PHFetchResult<PHAsset>!
+      let options = PHFetchOptions()
+      options.fetchLimit = quantity
+      options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+      fetchResult = PHAsset.fetchAssets(with: options)
+      
+      let photos = self.fetchResultAssetsToJs(fetchResult)
+      
+      call.success([
+        "photos": photos
+      ])
+    }
+
   }
   
   @objc func saveToPhotos(_ call: AVCPluginCall) {
@@ -40,6 +45,20 @@ public class Photos : AVCPlugin {
     }
   }
 
+  func checkAuthorization(_ call: AVCPluginCall, _ processBlock: @escaping () -> Void) {
+    let status = PHPhotoLibrary.authorizationStatus()
+    if status == PHAuthorizationStatus.authorized {
+      processBlock()
+    } else {
+      PHPhotoLibrary.requestAuthorization({ (newStatus) in
+        if newStatus == PHAuthorizationStatus.authorized {
+          processBlock()
+        } else {
+          call.error("User denied access")
+        }
+      })
+    }
+  }
 }
 
 
