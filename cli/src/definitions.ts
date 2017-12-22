3ccc8fc94029d1469951b6ee86a7ec69d25705d7
Max Lynch <max@drifty.com>
2017-12-22 11:00:41 -0600

    Add definitions file

---
+++ cli/src/definitions.ts
@@ -0,0 +1,50 @@
+export interface PackageJson {
+  name: string;
+  version: string;
+}
+
+
+export interface ExternalConfig {
+  webDir: string;
+  startPage: string;
+}
+
+export interface CliConfigPlatformAssets {
+  templateName: string;
+  templateDir?: string;
+}
+
+export interface CliConfigPlatform {
+  name: string;
+  minVersion: string;
+  platformDir: string;
+  webDir: string;
+  assets: CliConfigPlatformAssets;
+}
+
+export interface CliConfigCli {
+  binDir: string;
+  rootDir: string;
+  assetsName: string;
+  assetsDir: string;
+  package: PackageJson
+}
+
+export interface CliConfigApp {
+  rootDir: string;
+  webDir: string;
+  symlinkWebDir: boolean;
+  package: PackageJson,
+  extConfigName: string;
+  extConfigFilePath: string;
+  extConfig: ExternalConfig,
+  assets: CliConfigPlatformAssets;
+}
+
+
+export interface CliConfig {
+  android: CliConfigPlatform;
+  ios: CliConfigPlatform;
+  cli: CliConfigCli;
+  app: CliConfigApp;
+}
