b9cd44d25881883e5cad37711abc0b3192cb3631
Max Lynch <max@drifty.com>
2017-12-22 11:14:14 -0600

    Add app to copy assets

--- cli/scripts/copy-assets.js
+++ cli/scripts/copy-assets.js
@@ -4,6 +4,10 @@ const path = require('path');
 const MONOREPO_ROOT = path.join(__dirname, '../..');
 const ASSETS_DIST = path.join(__dirname, '../assets');
 
+const APP_TEMPLATE = 'app-template';
+const APP_TEMPLATE_SRC = path.join(MONOREPO_ROOT, APP_TEMPLATE);
+const APP_TEMPLATE_DST = path.join(ASSETS_DIST, APP_TEMPLATE);
+
 const ANDROID_TEMPLATE = 'android-template';
 const ANDROID_TEMPLATE_SRC = path.join(MONOREPO_ROOT, ANDROID_TEMPLATE);
 const ANDROID_TEMPLATE_DST = path.join(ASSETS_DIST, ANDROID_TEMPLATE);
@@ -14,6 +18,7 @@ const IOS_TEMPLATE_DST = path.join(ASSETS_DIST, IOS_TEMPLATE);
 
 fs.emptyDirSync(ASSETS_DIST);
 
+fs.copy(APP_TEMPLATE_SRC, APP_TEMPLATE_DST);
 fs.copy(ANDROID_TEMPLATE_SRC, ANDROID_TEMPLATE_DST);
 fs.copy(IOS_TEMPLATE_SRC, IOS_TEMPLATE_DST);
 
