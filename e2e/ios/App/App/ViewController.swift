c9ecb05733193c7140ca0d3f6ade50ef6014362f
Max Lynch <max@drifty.com>
2017-12-23 17:25:35 -0600

    e2e app structure

---
+++ e2e/ios/App/App/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Max Lynch on 12/22/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
