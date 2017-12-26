75a4a4e63d04a21cd46c7b10c7b549a2779af8cb
Max Lynch <max@drifty.com>
2017-12-26 14:59:18 -0600

    Site API content

--- site/src/components/site-header/site-header.tsx
+++ site/src/components/site-header/site-header.tsx
@@ -12,14 +12,12 @@ export class SiteHeader {
           <div class="logo"></div>
         </stencil-route-link>
         <div class="pull-right">
-          {/*
           <stencil-route-link urlMatch="/docs" url="/docs/intro/">
             Docs
           </stencil-route-link>
           <a href="https://github.com/ionic-team/avocado">
             GitHub
           </a>
-          */}
         </div>
       </div>
     );
