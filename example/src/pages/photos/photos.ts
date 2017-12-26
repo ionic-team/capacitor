228df0662bd3bf2aa241be0140019e8337424918
Max Lynch <max@drifty.com>
2017-12-26 12:22:15 -0600

    Photos work

--- example/src/pages/photos/photos.ts
+++ example/src/pages/photos/photos.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-import { Plugins } from '@avocadojs/core';
+import { Plugins, PhotosResult } from '@avocadojs/core';
 
 /**
  * Generated class for the PhotosPage page.
@@ -16,8 +17,9 @@ import { Plugins } from '@avocadojs/core';
   templateUrl: 'photos.html',
 })
 export class PhotosPage {
+  photos = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public sanitizer: DomSanitizer) {
   }
 
   ionViewDidLoad() {
@@ -26,7 +28,25 @@ export class PhotosPage {
 
   async loadPhotos() {
     var photos = await Plugins.Photos.getPhotos();
-    console.log('Loaded photos', photos);
+    this.printPhotos(photos);
+    this.photos = photos.photos.map(photo => {
+      return {
+        ...photo,
+        safeUrl: this.sanitizer.bypassSecurityTrustResourceUrl('data:image/png;base64,' + photo.data)
+      }
+    })
   }
 
+  printPhotos(photos) {
+    for(let photo of this.photos) {
+      console.log({
+        identifier: photo.identifier,
+        fullWidth: photo.fullWidth,
+        fullHeight: photo.fullHeight,
+        thumbnailWidth: photo.thumbnailWidth,
+        thumbnailHeight: photo.thumbnailHeight,
+        thumbnailBytes: photo.data.length
+      });
+    }
+  }
 }
--- ios/Avocado/Avocado/Plugins/Photos/Photos.swift
+++ ios/Avocado/Avocado/Plugins/Photos/Photos.swift
@@ -5,46 +5,94 @@ import Photos
 public class Photos : AVCPlugin {
   static let DEFAULT_QUANTITY = 25
   static let DEFAULT_TYPES = "photos"
+  static let DEFAULT_THUMBNAIL_WIDTH = 256
+  static let DEFAULT_THUMBNAIL_HEIGHT = 256
   
-  func fetchResultAssetsToJs(_ result: PHFetchResult<PHAsset>) -> [JSObject] {
+  var imageManager = PHCachingImageManager()
+  
+  func fetchResultAssetsToJs(_ call: AVCPluginCall, result: PHFetchResult<PHAsset>) {
     var assets: [JSObject] = []
     
+    //let after = call.getString("after")
+    let types = call.getString("types") ?? Photos.DEFAULT_TYPES
+    let thumbnailWidth = call.getInt("thumbnailWidth", defaultValue: Photos.DEFAULT_THUMBNAIL_WIDTH)!
+    let thumbnailHeight = call.getInt("thumbnailHeight", defaultValue: Photos.DEFAULT_THUMBNAIL_HEIGHT)!
+    let thumbnailSize = CGSize(width: thumbnailWidth, height: thumbnailHeight)
+    let thumbnailQuality = call.getInt("thumbnailQuality", defaultValue: 95)!
+    
+    let requestOptions = PHImageRequestOptions()
+    requestOptions.isNetworkAccessAllowed = true
+    requestOptions.version = .current
+    requestOptions.deliveryMode = .opportunistic
+    requestOptions.isSynchronous = true
+    
     result.enumerateObjects({ (asset, count: Int, stop: UnsafeMutablePointer<ObjCBool>) in
+      
+      if asset.mediaType == .image && types == "videos" {
+        return
+      }
+      if asset.mediaType == .video && types == "photos" {
+        return
+      }
+      
       print("Got asset item", asset, count)
       var a = JSObject()
-      if asset.creationDate != nil {
-        a["createdAt"] = JSDate.toString(asset.creationDate!)
-      }
-      a["location"] = JSObject()
-      var loc = a["location"] as! JSObject
-      loc["latitude"] = asset.location?.coordinate.latitude
-      loc["longitude"] = asset.location?.coordinate.longitude
-      // TODO: Expose more fields
-      assets.append(a)
+
+      self.imageManager.requestImage(for: asset, targetSize: thumbnailSize, contentMode: .aspectFill, options: requestOptions, resultHandler: { (fetchedImage, _) in
+        guard let image = fetchedImage else {
+          return
+        }
+        
+        a["identifier"] = asset.localIdentifier
+        
+        // TODO: We need to know original type
+        a["data"] = UIImageJPEGRepresentation(image, CGFloat(thumbnailQuality) / 100.0)?.base64EncodedString()
+        
+        if asset.creationDate != nil {
+          a["createdAt"] = JSDate.toString(asset.creationDate!)
+        }
+        a["fullWidth"] = asset.pixelWidth
+        a["fullHeight"] = asset.pixelHeight
+        a["thumbnailWidth"] = image.size.width
+        a["thumbnailHeight"] = image.size.height
+        a["location"] = self.makeLocation(asset)
+
+        assets.append(a)
+      })
     })
     
-    return assets
+    call.success([
+      "photos": assets
+    ])
   }
-  
+
+  @objc func getFullsizeUrls(_ call: AVCPluginCall) {
+    /*
+    guard let identifiers = call.getArray("identifiers") else {
+      call.error("Must provide identifiers array")
+      return
+    }
+    
+    let fetchResult = PHAsset.fetchAssets(withLocalIdentifiers: identifiers, options: <#T##PHFetchOptions?#>)
+    asset.requestContentEditingInput(with: PHContentEditingInputRequestOptions(), completionHandler: { (contentEditingInput, _) in
+      a["url"] = contentEditingInput?.fullSizeImageURL ?? ""
+    */
+  }
   
   @objc func getPhotos(_ call: AVCPluginCall) {
     let quantity = call.getInt("quantity", defaultValue: Photos.DEFAULT_QUANTITY)!
-    let after = call.getString("after")
-    let types = call.getString("types") ?? Photos.DEFAULT_TYPES
-  
-    checkAuthorization(call) {
+    checkAuthorization(allowed: {
       var fetchResult: PHFetchResult<PHAsset>!
       let options = PHFetchOptions()
       options.fetchLimit = quantity
       options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
       fetchResult = PHAsset.fetchAssets(with: options)
       
-      let photos = self.fetchResultAssetsToJs(fetchResult)
-      
-      call.success([
-        "photos": photos
-      ])
-    }
+      self.fetchResultAssetsToJs(call,
+                                 result: fetchResult)
+    }, notAllowed: {
+      call.error("Access to photos not allowed by user")
+    })
 
   }
   
@@ -55,20 +103,40 @@ public class Photos : AVCPlugin {
     }
   }
 
-  func checkAuthorization(_ call: AVCPluginCall, _ processBlock: @escaping () -> Void) {
+  func checkAuthorization(allowed: @escaping () -> Void, notAllowed: @escaping () -> Void) {
     let status = PHPhotoLibrary.authorizationStatus()
     if status == PHAuthorizationStatus.authorized {
-      processBlock()
+      allowed()
     } else {
       PHPhotoLibrary.requestAuthorization({ (newStatus) in
         if newStatus == PHAuthorizationStatus.authorized {
-          processBlock()
+          allowed()
         } else {
-          call.error("User denied access")
+          notAllowed()
         }
       })
     }
   }
+  
+  func makeLocation(_ asset: PHAsset) -> JSObject {
+    var loc = JSObject()
+    guard let location = asset.location else {
+      return loc
+    }
+    
+    loc["latitude"] = location.coordinate.latitude
+    loc["longitude"] = location.coordinate.longitude
+    loc["altitude"] = location.altitude
+    loc["heading"] = location.course
+    loc["speed"] = location.speed
+    return loc
+  }
+  
+  /*
+  deinit {
+    PHPhotoLibrary.shared().unregisterChangeObserver(self)
+  }
+ */
 }
 
 
