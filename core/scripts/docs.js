db87e579fb63cd3fd1971df2b484077eedf8e42f
Max Lynch <max@drifty.com>
2017-12-26 22:17:33 -0600

    Generate signature line

--- core/scripts/docs.js
+++ core/scripts/docs.js
@@ -26,13 +26,43 @@ const generateDocumentationForPlugin = (plugin) => {
 };
 
 const generateMethod = (method) => {
-  console.log(method.name);
-  const s = method.signatures[0];
-  const params = s.parameters;
-  params && params.forEach(param => {
-    console.log(param);
+  const parts = [method.name, '('];
+  const signature = method.signatures[0];
+  const params = signature.parameters;
+  params && params.forEach((param, i) => {
+    parts.push(param.name)
+    parts.push(': ');
+    parts.push(getParamTypeName(param));
+    if(i < params.length-1) {
+      parts.push(', ');
+    }
+  });
+  parts.push('): ');
 
-  })
+  const returnType = signature.type;
+  parts.push(getReturnTypeName(returnType));
+  const signatureLine = parts.join('');
+  console.log(signatureLine);
 }
+
+const getParamTypeName = (param) => {
+  const t = param.type.type;
+  if(t == 'reference') {
+    return param.type.name;
+  }
+  return 'any';
+};
+
+const getReturnTypeName = (returnType) => {
+  const r = returnType;
+  const type = `${r.name}`;
+
+  if(r.typeArguments) {
+    const typeArgParts = r.typeArguments.map(a => a.name);
+    return type + `<${typeArgParts.join(',')}>`;
+  }
+
+  return type;
+};
 
 plugins.forEach(plugin => generateDocumentationForPlugin(plugin));
