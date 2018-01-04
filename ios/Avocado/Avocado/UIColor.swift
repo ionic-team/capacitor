b96dd7ab82275247e302d472bd97cec43a3c7a75
Max Lynch <max@drifty.com>
2018-01-04 12:24:21 -0600

    Add toolbarColor to browser for iOS

---
+++ ios/Avocado/Avocado/UIColor.swift
@@ -0,0 +1,19 @@
+extension UIColor {
+  convenience init(fromHex hex: String) {
+    var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+    
+    if (cString.hasPrefix("#")) {
+      cString.remove(at: cString.startIndex)
+    }
+
+    var rgbValue:UInt32 = 0
+    Scanner(string: cString).scanHexInt32(&rgbValue)
+    
+    self.init(
+      red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+      green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+      blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+      alpha: CGFloat(1.0)
+    )
+  }
+}
