9176db997245152a730960ac3aa9c229d627e61d
Max Lynch <max@drifty.com>
2018-01-03 20:51:11 -0600

    Finished native modals for browser. Closes #35

--- android/avocado/src/main/java/com/avocadojs/BridgeWebChromeClient.java
+++ android/avocado/src/main/java/com/avocadojs/BridgeWebChromeClient.java
@@ -1,5 +1,6 @@
 package com.avocadojs;
 
+import android.webkit.JsPromptResult;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
@@ -13,6 +14,38 @@ public class BridgeWebChromeClient extends WebChromeClient {
       public void onResult(boolean value, boolean didCancel, String inputValue) {
         if(value) {
           result.confirm();
+        } else {
+          result.cancel();
+        }
+      }
+    });
+
+    return true;
+  }
+
+  @Override
+  public boolean onJsConfirm(WebView view, String url, String message, final JsResult result) {
+    Dialogs.confirm(view.getContext(), message, new Dialogs.OnResultListener() {
+      @Override
+      public void onResult(boolean value, boolean didCancel, String inputValue) {
+        if(value) {
+          result.confirm();
+        } else {
+          result.cancel();
+        }
+      }
+    });
+
+    return true;
+  }
+
+  @Override
+  public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, final JsPromptResult result) {
+    Dialogs.prompt(view.getContext(), message, new Dialogs.OnResultListener() {
+      @Override
+      public void onResult(boolean value, boolean didCancel, String inputValue) {
+        if(value) {
+          result.confirm(inputValue);
         } else {
           result.cancel();
         }
