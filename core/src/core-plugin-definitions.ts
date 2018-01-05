035844d2b46603f1502748a4571888178405bddf
Max Lynch <max@drifty.com>
2018-01-05 11:54:05 -0600

    CancellableCallback

--- core/src/core-plugin-definitions.ts
+++ core/src/core-plugin-definitions.ts
@@ -24,7 +24,15 @@ declare global {
 
 export type ISODateString = string;
 
-export interface Cancellable {
+/**
+ * CancellableCallback is a simple wrapper that a method will
+ * return to make it easy to cancel any repeated callback the method
+ * might have set up. For example: a geolocation watch.
+ */
+export interface CancellableCallback {
+  /**
+   * The cancel function for this method
+   */
   cancel: Function;
 }
 //
