6098f5c7282db072586e2b12f9237843397b7def
Max Lynch <max@drifty.com>
2018-01-11 15:34:18 -0600

    Android implemention for Accessibility.speak

--- core/src/core-plugin-definitions.ts
+++ core/src/core-plugin-definitions.ts
@@ -47,13 +47,25 @@ export interface AccessibilityPlugin {
    * Speak a string with a connected screen reader.
    * @param value the string to speak
    */
-  speak(options: { value: string }): Promise<void>;
+  speak(options: AccessibilitySpeakOptions): Promise<void>;
 
   /**
    * Listen for screen reader state change (on/off)
    */
   addListener(eventName: 'accessibilityScreenReaderStateChange', listenerFunc: ScreenReaderStateChangeCallback): PluginListenerHandle;
 }
+
+export interface AccessibilitySpeakOptions {
+  /**
+   * The string to speak
+   */
+  value: string;
+  /**
+   * The language to speak the string in, as its [ISO 639-1 Code](https://www.loc.gov/standards/iso639-2/php/code_list.php) (ex: "en")
+   */
+  language?: string;
+}
+
 export interface ScreenReaderEnabledResult {
   value: boolean;
 }
