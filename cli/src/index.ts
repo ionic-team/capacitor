b2b8a31a616ce2877a5888a8d037bb55a5fb7b77
Max Lynch <max@drifty.com>
2018-01-10 21:54:47 -0600

    ⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -76,7 +76,7 @@ export function run(process: NodeJS.Process, cliBinDir: string) {
   program.parse(process.argv);
 
   if (!program.args.length) {
-    console.log('\n  🥑');
+    console.log('\n  ⚡️');
     program.help();
   }
 }
