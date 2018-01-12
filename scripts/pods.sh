1e6ca2b00c133871aeeba6123ec321cfe3647445
Max Lynch <max@drifty.com>
2018-01-12 16:28:09 -0600

    More logging for bridgeviewcontroller and scripts for publishing cocoapods

---
+++ scripts/pods.sh
@@ -0,0 +1,11 @@
+# Make sure the version numbers are properly incremented in each podspect
+# and that a tag corresponding to the version was pushed to github!
+
+# Publish CapacitorCordova first
+pod trunk push CapacitorCordova.podspec --allow-warnings
+
+# Needed to ensure we can "see" the updated version of CapacitorCordova
+pod repo update
+
+# Finally, publish Capacitor
+pod trunk push Capacitor.podspec --allow-warnings
