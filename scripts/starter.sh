73554ab596444887ae8adb67429fe242151b15f2
Max Lynch <max@drifty.com>
2018-01-21 18:03:04 -0600

    Script to update and deploy starter

--- scripts/starter.sh
+++ scripts/starter.sh
@@ -3,13 +3,14 @@ LAST_COMMIT_MESSAGE=`git log -1 --pretty=format:"%s" -- starter/`
 
 echo $LAST_COMMIT_MESSAGE
 
-if [ "$#" == "0" ]; then
+if [ "$#" -ne "1" ]; then
 	echo "$USAGE"
 	exit 1
 fi
+STARTER_REPO=$1
+echo $STARTER_REPO
 
-STARTER_REPO=$0
-cp -R starter/* STARTER_REPO
+cp -R starter/* $STARTER_REPO
 cd $STARTER_REPO
 git add .
 git commit -m "$LAST_COMMIT_MESSAGE"
