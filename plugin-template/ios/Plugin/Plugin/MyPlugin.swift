46358084d76e5c5d1288f0a56895d9a675fe9b22
Max Lynch <max@drifty.com>
2018-01-29 07:10:21 -0600

    Plugin template ios

---
+++ plugin-template/ios/Plugin/Plugin/MyPlugin.swift
@@ -0,0 +1,13 @@
+import Foundation
+import Capacitor
+
+@objc(MyPlugin)
+public class MyPlugin : CAPPlugin {
+    
+    @objc func echo(_ call: CAPPluginCall) {
+        let value = call.getString("value") ?? ""
+        call.success([
+            "value": value
+        ])
+    }
+}
