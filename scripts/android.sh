3b5007846de22ff9f34dcae6ac140950fca95565
Max Lynch <max@drifty.com>
2018-01-31 10:40:54 -0600

    Lerna publish android

--- scripts/android.sh
+++ scripts/android.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
+
+LERNA_JSON=`cat lerna.json`;
+LERNA_VERSION="$(node -pe "JSON.parse(\`$LERNA_JSON\`)['version']")"
+
+echo "Deploying android v$LERNA_VERSION"
+
 cd android
+
+export BINTRAY_PKG_VERSION=$LERNA_VERSION
 
 ./gradlew clean build -b capacitor/build.gradle bintrayUpload -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY -PdryRun=false
