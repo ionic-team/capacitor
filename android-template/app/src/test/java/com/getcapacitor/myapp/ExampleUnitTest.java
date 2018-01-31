edb20b7d792caf202aee8fe21a03321769d9bf2a
Max Lynch <max@drifty.com>
2018-01-31 10:04:50 -0600

    Formatting

--- android-template/app/src/test/java/com/getcapacitor/myapp/ExampleUnitTest.java
+++ android-template/app/src/test/java/com/getcapacitor/myapp/ExampleUnitTest.java
@@ -10,8 +10,8 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
+  @Test
+  public void addition_isCorrect() throws Exception {
+    assertEquals(4, 2 + 2);
+  }
 }
