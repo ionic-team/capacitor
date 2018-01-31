f5c8097332ccb9db78fdcee369606a722777a7f4
Max Lynch <max@drifty.com>
2018-01-31 17:27:51 -0600

    Working deploy script w/ tags and all that jazz

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,21 @@
-if [ ! -z $BINTRAY_USER]; then
+if [ -z $BINTRAY_USER ]; then
   echo "Must define BINTRAY_USER and BINTRAY_KEY env var before deploying. See https://github.com/ionic-team/capacitor/blob/master/.github/CONTRIBUTING.md"
 fi
 
 LERNA_JSON=`cat lerna.json`;
 export LERNA_VERSION="$(node -pe "JSON.parse(\`$LERNA_JSON\`)['version']")"
 
-if [ ! -z $STARTER_PATH]; then
+if [ -z $STARTER_PATH ]; then
   STARTER_PATH=../capacitor-starter
 fi
 
 echo "Deploying Capacitor v$LERNA_VERSION"
 
-bash scripts/deploy/pods.sh
-bash scripts/deploy/android.sh
-bash scripts/deploy/starter.sh ../capacitor-starter
+export POD_VERSION=$LERNA_VERSION
+
+# Update the version number in the podspec
+sed "s/POD_VERSION/'$POD_VERSION'/g" scripts/deploy/Capacitor.podspec.template > Capacitor.podspec
+sed "s/POD_VERSION/'$POD_VERSION'/g" scripts/deploy/CapacitorCordova.podspec.template > CapacitorCordova.podspec
 
 # This is what we do instead of letting lerna git commit for us
 git add lerna.json
@@ -21,7 +23,11 @@ git add Capacitor.podspec
 git add CapacitorCordova.podspec
 git add cli/package.json
 git add core/package.json
-git tag $LERNA_VERSION
-git push --tags
 git commit -m "Release v$LERNA_VERSION"
-git push origin master
+git tag $LERNA_VERSION -m $LERNA_VERSION
+git push --follow-tags origin master
+
+# Do the actual native deploys second, because they require tags/releases in github
+bash scripts/deploy/pods.sh
+bash scripts/deploy/android.sh
+bash scripts/deploy/starter.sh ../capacitor-starter
