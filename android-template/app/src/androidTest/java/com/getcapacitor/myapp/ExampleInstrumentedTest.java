dd1c9c814224a4f288f6b007f5adbb12422c9688
Max Lynch <max@drifty.com>
2018-01-31 10:04:50 -0600

    Formatting

--- android-template/app/src/androidTest/java/com/getcapacitor/myapp/ExampleInstrumentedTest.java
+++ android-template/app/src/androidTest/java/com/getcapacitor/myapp/ExampleInstrumentedTest.java
@@ -16,11 +16,11 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+  @Test
+  public void useAppContext() throws Exception {
+    // Context of the app under test.
+    Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.getcapacitor.app", appContext.getPackageName());
-    }
+    assertEquals("com.getcapacitor.app", appContext.getPackageName());
+  }
 }
--- android-template/app/src/main/java/com/getcapacitor/myapp/MainActivity.java
+++ android-template/app/src/main/java/com/getcapacitor/myapp/MainActivity.java
@@ -5,8 +5,8 @@ import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+                                                  super.onCreate(savedInstanceState);
     }
 }
