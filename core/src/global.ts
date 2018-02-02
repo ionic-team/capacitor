706061940d2240eae39beec298e162dd4415d4c7
Max Lynch <max@drifty.com>
2018-02-02 16:53:03 -0600

    Export capacitor on window

--- core/src/global.ts
+++ core/src/global.ts
@@ -7,6 +7,11 @@ var Capacitor:_Capacitor = {
 declare var window: any;
 Capacitor = window.Capacitor || Capacitor;
 
+// Export window.Capacitor if not available already (ex: web)
+if (!window.Capacitor) {
+  window.Capacitor = Capacitor;
+}
+
 const Plugins = Capacitor.Plugins;
 
 export { Capacitor, Plugins };
