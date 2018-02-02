45c8f0d630fc7cc9fa071fe60aab4b5d607cdb74
Julio César <jcesarmobile@gmail.com>
2018-02-02 18:43:14 +0100

    Fix example build

--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
