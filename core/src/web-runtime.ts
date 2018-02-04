3611968612b063878a742c6f0f8794daed900014
Max Lynch <max@drifty.com>
2018-02-04 16:22:06 -0600

    Proxy for missing plugins on the web

--- core/src/web-runtime.ts
+++ core/src/web-runtime.ts
@@ -3,6 +3,34 @@ export class CapacitorWeb {
   platform = 'web';
   isNative = false;
 
+  NOOP_PLUGIN: any = {};
+
+  constructor() {
+    this.NOOP_PLUGIN = new Proxy<any>(this.NOOP_PLUGIN, {
+      get: (target, prop) => {
+        if (typeof target[prop] === 'undefined') {
+          return this.pluginMethodNoop.bind(this, target, prop);
+        } else {
+          return target[prop];
+        }
+      }
+    });
+
+    this.Plugins = new Proxy<any>(this.Plugins, {
+      get: (target, prop) => {
+        if (typeof target[prop] === 'undefined') {
+          return this.NOOP_PLUGIN;
+        } else {
+          return target[prop];
+        }
+      }
+    })
+  }
+
+  pluginMethodNoop(_target: any, _prop: PropertyKey) {
+    return Promise.reject(`Plugin does not have web implementation.`);
+  }
+
   getPlatform() {
     return this.platform;
   }
