169d57d92afbbd44a230a2501e3cc3bba300ac9a
Max Lynch <max@drifty.com>
2018-02-16 09:40:20 -0600

    Create local properties and sync for Android on add

--- cli/src/util/fs.ts
+++ cli/src/util/fs.ts
@@ -11,10 +11,17 @@ export const cpAsync = util.promisify(fsExtra.copy);
 export const symlinkAsync = util.promisify(fs.symlink);
 export const readFileAsync = util.promisify(fs.readFile);
 export const writeFileAsync = util.promisify(fs.writeFile);
-export const existsAsync = util.promisify(fs.exists);
 export const readdirAsync = util.promisify(fs.readdir);
 export const statAsync = util.promisify(fs.stat);
 export const lstatAsync = util.promisify(fs.lstat);
 export const removeSync = fsExtra.removeSync;
 export const ensureDirSync = fsExtra.ensureDirSync;
 export const copySync = fsExtra.copySync;
+export const existsAsync = async (path: string) => {
+  try {
+    const stat = await statAsync(path);
+    return true;
+  } catch {
+    return false;
+  }
+};
