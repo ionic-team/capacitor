601065287b929bd5f7713226542d6490ebc5a475
Max Lynch <max@drifty.com>
2018-02-22 21:45:43 -0600

    Storage API iOS

--- ios/Capacitor/Capacitor/Plugins/Storage.swift
+++ ios/Capacitor/Capacitor/Plugins/Storage.swift
@@ -5,13 +5,23 @@ import Foundation
  */
 @objc(CAPStoragePlugin)
 public class CAPStoragePlugin: CAPPlugin {
+  let KEY_PREFIX = "_cap_"
+  
+  public override func load() {
+  }
+  
+  func getDefaults() -> UserDefaults {
+    return UserDefaults.standard
+  }
+  
   @objc func get(_ call: CAPPluginCall) {
     guard let key = call.getString("key") else {
       call.error("Must provide a key")
       return
     }
-    let value: String? = UserDefaults.standard.string(forKey: key)
-    call.success([
+
+    let value: String? = getDefaults().string(forKey: makeKey(key))
+    call.resolve([
       "value": value
     ])
   }
@@ -24,9 +34,9 @@ public class CAPStoragePlugin: CAPPlugin {
     let value = call.getString("value", "")
     
     
-    UserDefaults.standard.set(value, forKey: key)
+    getDefaults().set(value, forKey: makeKey(key))
     
-    call.success()
+    call.resolve()
   }
   
   @objc func remove(_ call: CAPPluginCall) {
@@ -35,21 +45,43 @@ public class CAPStoragePlugin: CAPPlugin {
       return
     }
 
-    UserDefaults.standard.removeObject(forKey: key)
+    getDefaults().removeObject(forKey: makeKey(key))
     
-    call.success()
+    call.resolve()
   }
   
   @objc func keys(_ call: CAPPluginCall) {
-    let keys = Array(UserDefaults.standard.persistentDomain(forName: Bundle.main.bundleIdentifier!)!.keys)
-    call.success([
+    let keys = getDefaults().dictionaryRepresentation().keys.filter({ (key) -> Bool in
+      return isKey(key)
+    }).map({ (key) -> String in
+      return getKey(key)
+    })
+    
+    call.resolve([
       "keys":  keys
     ])
   }
   
   @objc func clear(_ call: CAPPluginCall) {
-    let appDomain = Bundle.main.bundleIdentifier!
-    UserDefaults.standard.removePersistentDomain(forName: appDomain)
+    getDefaults().dictionaryRepresentation().keys.filter({ (key) -> Bool in
+      return isKey(key)
+    }).forEach { (key) in
+      getDefaults().removeObject(forKey: key)
+    }
+    call.resolve()
+  }
+  
+  func makeKey(_ key: String) -> String {
+    return KEY_PREFIX + key
+  }
+  
+  func isKey(_ key: String) -> Bool {
+    return key.hasPrefix(KEY_PREFIX)
+  }
+  
+  func getKey(_ key: String) -> String {
+    let prefixLen = KEY_PREFIX.count
+    return String(key.dropFirst(prefixLen))
   }
 }
 
