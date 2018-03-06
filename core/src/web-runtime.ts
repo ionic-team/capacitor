15738f3b7dc2440a0be04b712552132d86d550a4
Max Lynch <max@drifty.com>
2018-03-06 08:21:15 -0600

    Update web-runtime.ts

--- core/src/web-runtime.ts
+++ core/src/web-runtime.ts
@@ -3,39 +3,21 @@ export class CapacitorWeb {
   platform = 'web';
   isNative = false;
 
-  NOOP_PLUGIN: any = {};
-
   constructor() {
-    // Construct a "Noop Plugin" that handles all method calls if a plugin
-    // isn't available, returning a rejected promise
-    /*
-    this.NOOP_PLUGIN = new Proxy<any>(this.NOOP_PLUGIN, {
-      get: (target, prop) => {
-        console.log('NOOP:: target: ' + JSON.stringify(target) + '  ---  prop: ' + JSON.stringify(prop));
-        if (typeof target[prop] === 'undefined') {
-          return this.pluginMethodNoop.bind(this, target, prop);
-        } else {
-          return target[prop];
-        }
-      }
-    });
-    */
-
     // Build a proxy for the Plugins object that returns the "Noop Plugin"
     // if a plugin isn't available
     this.Plugins = new Proxy<any>(this.Plugins, {
       get: (target, prop) => {
         if (typeof target[prop] === 'undefined') {
-          //return this.NOOP_PLUGIN;
           let thisRef = this;
           return new Proxy<any>({}, {
-              get: (_target, _prop) => {
-                  if (typeof _target[_prop] === 'undefined') {
-                      return thisRef.pluginMethodNoop.bind(thisRef, _target, _prop,  prop);
-                  } else {
-                      return _target[_prop];
-                  }
+            get: (_target, _prop) => {
+              if (typeof _target[_prop] === 'undefined') {
+                return thisRef.pluginMethodNoop.bind(thisRef, _target, _prop,  prop);
+              } else {
+                return _target[_prop];
               }
+            }
           });
         } else {
           return target[prop];
