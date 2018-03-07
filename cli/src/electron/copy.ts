5df38d38472fa41030e8659b6b4f7ddc2888bcb5
IT-MikeS <tech@croftersorganic.com>
2018-03-07 08:43:58 -0500

    npx cap copy electron fixes

--- cli/src/electron/copy.ts
+++ cli/src/electron/copy.ts
@@ -1,13 +1,20 @@
-import { runCommand, runTask } from '../common';
+import { runTask } from '../common';
 import { Config } from '../config';
-import { copy } from 'fs-extra';
-import { join, relative, resolve } from 'path';
+import {copy, remove} from 'fs-extra';
+import {basename, join, relative, resolve} from 'path';
 
 export async function copyElectron(config: Config) {
   const chalk = require('chalk');
-  console.log('Copying runtime!');
+  const webAbsDir = config.app.webDirAbs;
+  const webRelDir = basename(webAbsDir);
+  const nativeRelDir = relative(config.app.rootDir, config.electron.webDirAbs);
   const runtimePath = resolve(config.app.rootDir, 'node_modules', '@capacitor/core', 'dist/capacitor.js');
-  return runTask(`Copying capacitor.js to electron app.`, () => {
+  return await runTask(`Copying web assets from ${chalk.bold(webRelDir)} to ${chalk.bold(nativeRelDir)}`, async () => {
+    console.log(`Cleaning ${config.electron.webDirAbs}...`);
+    await remove(config.electron.webDirAbs);
+    console.log(`Copying web assets...`);
+    await copy(webAbsDir, config.electron.webDirAbs);
+    console.log(`Copying runtime...`);
     return copy(runtimePath, join(config.electron.webDirAbs, 'capacitor.js'));
   });
 }
