111d8fd837d23d7762e23c73c27582bbe821f3e5
IT-MikeS <tech@croftersorganic.com>
2018-03-07 09:46:36 -0500

    more tweaks

--- electron-template/index.js
+++ electron-template/index.js
@@ -31,13 +31,12 @@ function createWindow () {
     width: 1600
   });
 
-  // Set our above template to the Menu Object if we are in development mode, dont want users having the devtools.
-  if (isDevMode)
+  if (isDevMode) {
+    // Set our above template to the Menu Object if we are in development mode, dont want users having the devtools.
     Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplateDev));
-
-  // If we are developers we might as well open the devtools by default.
-  if (isDevMode)
+    // If we are developers we might as well open the devtools by default.
     mainWindow.webContents.openDevTools();
+  }
 
   // Render our app onto the page.
   mainWindow.loadURL(`file://${__dirname}/app/index.html`);
@@ -52,15 +51,13 @@ app.on('ready', createWindow);
 app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+  if (process.platform !== 'darwin')
     app.quit();
-  }
 });
 
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
+  if (mainWindow === null)
     createWindow();
-  }
 });
