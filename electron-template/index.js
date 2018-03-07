61ed6c601333da46739235d2d72f3ff946c50d5c
IT-MikeS <tech@croftersorganic.com>
2018-03-07 07:44:51 -0500

    clean up

--- electron-template/index.js
+++ electron-template/index.js
@@ -51,7 +51,7 @@ app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
 });
 
@@ -59,6 +59,6 @@ app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
 });
