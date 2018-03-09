3b59dd2d2ffe1e7cf56fe23cab316761122d553d
IT-MikeS <tech@croftersorganic.com>
2018-03-09 12:10:23 -0500

    Testing electron injection

---
+++ electron/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+function getURLFileContents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, data) => {
+      // console.error(err);
+      if(err)
+        reject(err);
+      resolve(data.toString());
+    });
+  });
+}
+
+const injectCapacitor = async function(url) {
+  try {
+    // console.log(url.substr(url.indexOf('://') + 3));
+    let urlFileContents = await getURLFileContents(url.substr(url.indexOf('://') + 3));
+    let pathing = path.join(url.substr(url.indexOf('://') + 3), '../../node_modules/@capacitor/electron/inject.js').replace(/\\/g,'\\\\');
+    urlFileContents = urlFileContents.replace('<body>', `<body><script>window.require('${pathing}')</script>`);
+    return 'data:text/html;charset=UTF-8,' + urlFileContents;
+  } catch(e) {
+    // console.error(e);
+    return url;
+  }
+};
+
+module.exports = injectCapacitor;
