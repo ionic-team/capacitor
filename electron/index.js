fff3666f62b5bcceed0fe65c76002e0276b413ea
IT-MikeS <tech@croftersorganic.com>
2018-03-09 12:36:39 -0500

    comment out logs

--- electron/index.js
+++ electron/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 function getURLFileContents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, (err, data) => {
-      console.error(err);
+      // console.error(err);
       if(err)
         reject(err);
       resolve(data.toString());
@@ -14,15 +14,15 @@ function getURLFileContents(path) {
 
 const injectCapacitor = async function(url) {
   try {
-    console.log(url.substr(url.indexOf('://') + 3));
+    // console.log(url.substr(url.indexOf('://') + 3));
     let urlFileContents = await getURLFileContents(url.substr(url.indexOf('://') + 3));
     let pathing = path.join(url.substr(url.indexOf('://') + 3), '../../node_modules/@capacitor/electron/electron-bridge.js');
-    console.log(pathing);
+    // console.log(pathing);
     urlFileContents = urlFileContents.replace('<body>', `<body><script>window.require('${pathing.replace(/\\/g,'\\\\')}')</script>`);
-    console.log(urlFileContents);
+    // console.log(urlFileContents);
     return 'data:text/html;charset=UTF-8,' + urlFileContents;
   } catch(e) {
-    console.error(e);
+    // console.error(e);
     return url;
   }
 };
