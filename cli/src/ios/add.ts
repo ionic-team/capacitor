d20b847e8d00286aaf24c58e81f56a1ab6657e8d
Max Lynch <max@drifty.com>
2018-03-15 12:43:03 -0500

    Fix iOS @capacitor/ios install on add platform

--- cli/src/ios/add.ts
+++ cli/src/ios/add.ts
@@ -6,7 +6,9 @@ import { copy } from 'fs-extra';
 export const addIOSChecks: CheckFunction[] = [checkCocoaPods];
 
 export async function addIOS(config: Config) {
-  await runCommand(`npm install @capacitor/ios`);
+  await runTask(`Installing iOS dependencies`, async () => {
+    return runCommand(`cd "${config.app.rootDir}" && npm install --save @capacitor/ios`);
+  });
   await runTask(`Adding native xcode project in: ${config.ios.platformDir}`, () => {
     return copy(config.ios.assets.templateDir, config.ios.platformDir);
   });
