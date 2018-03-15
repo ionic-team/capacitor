c40d7bc8e6056b053b5e8fbd47b05f0f97b88f6b
Max Lynch <max@drifty.com>
2018-03-15 13:27:06 -0500

    Deploy content

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,6 +20,7 @@ git add Capacitor.podspec
 git add CapacitorCordova.podspec
 git add cli/package.json
 git add core/package.json
+git add electron/package.json
 git commit -m "Release v$LERNA_VERSION"
 git tag $LERNA_VERSION -m $LERNA_VERSION
 git push --follow-tags origin master
