f587e3272f5ccf585aec8e2c23a845f1f330c096
Max Lynch <max@drifty.com>
2018-03-15 14:20:05 -0500

    Updated copy assets to try to fix #344

--- cli/scripts/copy-assets.js
+++ cli/scripts/copy-assets.js
@@ -38,14 +38,30 @@ fs.emptyDirSync(ASSETS_DIST);
 fs.copySync(APP_TEMPLATE_SRC, APP_TEMPLATE_DST);
 // Rename our Android .iml file
 fs.copySync(ANDROID_TEMPLATE_SRC, ANDROID_TEMPLATE_DST);
-if (fs.existsSync(path.join(ANDROID_TEMPLATE_DST, 'android-template.iml'))) {
-  fs.moveSync(path.join(ANDROID_TEMPLATE_DST, 'android-template.iml'), path.join(ANDROID_TEMPLATE_DST, 'android.iml'));
-  let imlContent = fs.readFileSync(path.join(ANDROID_TEMPLATE_DST, 'android.iml'), 'utf8');
-  imlContent = imlContent.replace(/android-template/g, 'android');
-  fs.writeFileSync(path.join(ANDROID_TEMPLATE_DST, 'android.iml'), imlContent);
-}
+replaceAndroidModule();
 fs.copySync(IOS_TEMPLATE_SRC, IOS_TEMPLATE_DST);
 fs.copySync(ELECTRON_TEMPLATE_SRC, ELECTRON_TEMPLATE_DST);
 fs.copySync(PLUGIN_TEMPLATE_SRC, PLUGIN_TEMPLATE_DST);
 fs.copySync(ANDROID_PLUGINS_FOLDER_SRC, ANDROID_PLUGINS_FOLDER_DST);
 fs.copySync(IOS_PLUGINS_FOLDER_SRC, IOS_PLUGINS_FOLDER_DST);
+
+function replaceAndroidModule() {
+  if (fs.existsSync(path.join(ANDROID_TEMPLATE_DST, 'android-template.iml'))) {
+    fs.moveSync(path.join(ANDROID_TEMPLATE_DST, 'android-template.iml'), path.join(ANDROID_TEMPLATE_DST, 'android.iml'));
+    let imlContent = fs.readFileSync(path.join(ANDROID_TEMPLATE_DST, 'android.iml'), 'utf8');
+    imlContent = imlContent.replace(/android-template/g, 'android');
+    fs.writeFileSync(path.join(ANDROID_TEMPLATE_DST, 'android.iml'), imlContent);
+  }
+  const modulesXmlPath = path.join(ANDROID_TEMPLATE_DST, '.idea/modules.xml');
+  const workspaceXmlPath = path.join(ANDROID_TEMPLATE_DST, '.idea/workspace.xml');
+  if (fs.existsSync(modulesXmlPath)) {
+    let modulesXml = fs.readFileSync(modulesXmlPath, 'utf8');
+    modulesXml = modulesXml.replace(/android-template/g, 'android');
+    fs.writeFileSync(modulesXmlPath, modulesXml);
+  }
+  if (fs.existsSync(workspaceXmlPath)) {
+    let workspaceXml = fs.readFileSync(workspaceXmlPath, 'utf8');
+    workspaceXml = workspaceXml.replace(/android-template/g, 'android');
+    fs.writeFileSync(workspaceXmlPath, workspaceXml);
+  }
+}
