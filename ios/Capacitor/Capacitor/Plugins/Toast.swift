845e98bbe481abb3e5cd85ed253440bad9ecb0da
Max Lynch <max@drifty.com>
2018-03-23 15:25:10 -0500

    twas merely a derp

--- ios/Capacitor/Capacitor/Plugins/Toast.swift
+++ ios/Capacitor/Capacitor/Plugins/Toast.swift
@@ -1,39 +1,55 @@
 import Foundation
 
+
+
 @objc(CAPToastPlugin)
 public class CAPToastPlugin : CAPPlugin {
+  var toast: UILabel?
   @objc func show(_ call: CAPPluginCall) {
     guard let text = call.get("text", String.self) else {
       call.error("text must be provided and must be a string.")
       return
     }
-    let durationStyle = call.get("durationStyle", String.self, "short")!
-    let duration = durationStyle == "short" ? 1000 : 5000
+    let durationStyle = call.get("durationStyle", String.self, "long")!
+    let duration = durationStyle == "short" ? 1500 : 3000
     
     DispatchQueue.main.async {
       let vc = self.bridge!.viewController
       
-      let toastLabel = UILabel(frame: CGRect(x: vc.view.frame.size.width/2 - 75, y: vc.view.frame.size.height-100, width: 150, height: 35))
-      toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
-      toastLabel.textColor = UIColor.white
-      toastLabel.textAlignment = .center;
-      toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
-      toastLabel.text = text
-      toastLabel.alpha = 0
-      toastLabel.layer.cornerRadius = 10;
-      toastLabel.clipsToBounds  =  true
-      vc.view.addSubview(toastLabel)
+      let maxSizeTitle : CGSize = CGSize(width: vc.view.bounds.size.width-16, height: vc.view.bounds.size.height)
       
-      UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseIn, animations: {
-        toastLabel.alpha = 1.0
+      let lb = UILabel()
+      lb.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+      lb.textColor = UIColor.white
+      lb.textAlignment = .center;
+      lb.font = UIFont(name: "Montserrat-Light", size: 12.0)
+      lb.text = text
+      lb.alpha = 0
+      lb.layer.cornerRadius = 10;
+      lb.clipsToBounds  =  true
+      
+      var expectedSizeTitle : CGSize = lb.sizeThatFits(maxSizeTitle)
+      // UILabel can return a size larger than the max size when the number of lines is 1
+      let minWidth = min(maxSizeTitle.width, expectedSizeTitle.width)
+      let minHeight = min(maxSizeTitle.height, expectedSizeTitle.height)
+      expectedSizeTitle = CGSize(width: minWidth, height: minHeight)
+      lb.frame = CGRect(x:((vc.view.bounds.size.width)/2) - ((expectedSizeTitle.width+16)/2) , y: (vc.view.bounds.size.height/2) - ((expectedSizeTitle.height+16)/2), width: expectedSizeTitle.width+16, height: expectedSizeTitle.height+16)
+      
+      lb.padding = UIEdgeInsets(top: 4, left: 4, bottom: 4, right: 4)
+      self.toast = lb
+      
+      vc.view.addSubview(lb)
+      
+      UIView.animateKeyframes(withDuration: 0.3, delay: 0, animations: {
+        self.toast!.alpha = 1.0
       }, completion: {(isCompleted) in
-        DispatchQueue.main.async {
-          UIView.animate(withDuration: 1000, delay: Double(duration)/1000, options: .curveEaseOut, animations: {
-            toastLabel.alpha = 0.0
-          }, completion: {(isCompleted) in
-            toastLabel.removeFromSuperview()
-          })
-        }
+        
+        UIView.animate(withDuration: 0.3, delay: (Double(duration) / 1000), options: .curveEaseOut, animations: {
+          self.toast!.alpha = 0.0
+        }, completion: {(isCompleted) in
+          self.toast!.removeFromSuperview()
+        })
+
       })
     }
   }
