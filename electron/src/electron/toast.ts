1e2fe585f127b5e2b62b23b8de308ddc0e0ef404
IT-MikeS <tech@croftersorganic.com>
2018-03-29 16:10:22 -0400

    fix ToastController not present check

--- electron/src/electron/toast.ts
+++ electron/src/electron/toast.ts
@@ -8,29 +8,37 @@ export interface ToastShowOptions {
 
 export class ToastPluginElectron extends WebPlugin implements ToastPlugin {
 
+  toastControllerPresent:boolean;
+
   constructor() {
     super({
       name: 'Toast',
       platforms: ['electron']
     });
+    this.toastControllerPresent = true;
     let testel = document.querySelector('ion-toast-controller');
     if(testel == null) {
-      console.error()
+      console.error('No @ionic/core ToastController detected, please make sure you have included the web components.');
+      this.toastControllerPresent = false;
     }
   }
 
   async show(options: ToastShowOptions) {
-    const toastController:ToastController = document.querySelector('ion-toast-controller');
-    let duration = 3000;
-    if(options.duration) {
-      duration = options.duration.localeCompare('long') === 0 ? 5000 : 3000;
+    if(this.toastControllerPresent) {
+      const toastController: ToastController = document.querySelector('ion-toast-controller');
+      let duration = 3000;
+      if (options.duration) {
+        duration = options.duration.localeCompare('long') === 0 ? 5000 : 3000;
+      }
+      const toastElement = await toastController.create({
+        position: 'bottom',
+        message: options.title,
+        duration: duration,
+      });
+      return await toastElement.present();
+    } else {
+      return Promise.reject('No @ionic/core ToastController detected, please make sure you have included the web components.');
     }
-    const toastElement = await toastController.create({
-      position: 'bottom',
-      message: options.title,
-      duration: duration,
-    });
-    return await toastElement.present();
   }
 }
 
