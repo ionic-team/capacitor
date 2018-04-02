e84dad2b56a5e224794c377176c7ca68a09a61ba
Julio César <jcesarmobile@gmail.com>
2018-04-02 18:53:21 +0200

    Handle allowEditing for Android

--- android/capacitor/src/main/java/com/getcapacitor/PluginRequestCodes.java
+++ android/capacitor/src/main/java/com/getcapacitor/PluginRequestCodes.java
@@ -5,4 +5,5 @@ public class PluginRequestCodes {
   public static final int CAMERA_IMAGE_CAPTURE = 9002;
   public static final int CAMERA_IMAGE_PICK = 9003;
   public static final int GEOLOCATION_REQUEST_PERMISSIONS = 9004;
+  public static final int CAMERA_IMAGE_EDIT = 9005;
 }
