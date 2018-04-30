0887db89d3ffe08e9a257f23b42ec4d9c20f8603
techiediaries <techiediaries9@gmail.com>
2018-04-30 01:21:23 +0100

    Adding electron to example

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Plugins } from '@capacitor/core';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage = 'AppPage';
+  rootPage = 'ClipboardPage';
 
   PLUGINS = [
     { name: 'App', page: 'AppPage' },
