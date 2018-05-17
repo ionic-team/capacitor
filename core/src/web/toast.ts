9a56a6039d066c64ee6f66a9b2f30ed479e47051
Julio César <jcesarmobile@gmail.com>
2018-05-17 21:12:51 +0200

    Add web toast

---
+++ core/src/web/toast.ts
@@ -0,0 +1,36 @@
+import { WebPlugin } from './index';
+import { ToastPlugin, ToastShowOptions } from '../core-plugin-definitions';
+
+export class ToastPluginWeb extends WebPlugin implements ToastPlugin {
+
+  constructor() {
+    super({
+      name: 'Toast',
+      platforms: ['web']
+    });
+  }
+
+  async show(options: ToastShowOptions) {
+    var controller: any = document.querySelector('ion-toast-controller');
+    if (!controller) {
+      controller = document.createElement('ion-toast-controller');
+      document.body.appendChild(controller);
+    }
+    await controller.componentOnReady();
+   
+    let duration = 3000;
+    if (options.duration) {
+      duration = options.duration.localeCompare('long') === 0 ? 5000 : 3000;
+    }
+    const toast = await controller.create({
+      position: 'bottom',
+      message: options.text,
+      duration: duration,
+    });
+    return await toast.present();
+  }
+}
+
+const Toast = new ToastPluginWeb();
+
+export { Toast };
