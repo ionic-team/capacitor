c239ba41c48f937c45a99297613482295920e5b4
Ahmed <techiediaries9@gmail.com>
2018-06-20 19:17:35 +0100

    Add alert implementation

--- electron/src/electron/modals.ts
+++ electron/src/electron/modals.ts
@@ -1,5 +1,7 @@
 import { WebPlugin } from "@capacitor/core";
 
+declare var require: any
+const {dialog , getCurrentWindow} = require('electron').remote
 
 import {
   ModalsPlugin,
@@ -19,6 +21,17 @@ export class ModalsPluginElectron extends WebPlugin implements ModalsPlugin {
       platforms: ['electron']
     });
   }
+  async alert(options: AlertOptions): Promise<void> {
+    //window.alert(options.message); we can't set title and button title
+    const alert = (message: string, title: string = '') =>
+    {    
+        
+        let buttons = [options.buttonTitle || 'OK']
+        dialog.showMessageBox(getCurrentWindow(), {message, title, buttons});       
+    }
+        alert(options.message, options.title);
+        return Promise.resolve();
+  }  
 
 }
 
