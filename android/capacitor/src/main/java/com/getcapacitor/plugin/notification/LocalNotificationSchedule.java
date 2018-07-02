2d4a14e02c7f0a43a60b1472aa99df255e14f47c
Wojciech Trocki <wtrocki@redhat.com>
2018-07-02 16:36:40 +0100

    Add scheduled notification support

--- android/capacitor/src/main/java/com/getcapacitor/plugin/notification/LocalNotificationSchedule.java
+++ android/capacitor/src/main/java/com/getcapacitor/plugin/notification/LocalNotificationSchedule.java
@@ -1,14 +1,41 @@
 package com.getcapacitor.plugin.notification;
 
+import android.text.format.DateUtils;
+
+import com.getcapacitor.plugin.common.JsonParserUtils;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class LocalNotificationSchedule {
   private Date at;
-  private Boolean repeat;
+  private Boolean repeats;
   private String every;
-  private JSONObject on;
+  // TODO `on` field support
+
+  public LocalNotificationSchedule(JSONObject jsonNotification) throws ParseException {
+    JSONObject schedule = null;
+    try {
+      schedule = jsonNotification.getJSONObject("schedule");
+    } catch (JSONException e) {
+      return;
+    }
+    this.repeats = JsonParserUtils.getBoolean("repeats", schedule);
+    // 'year'|'month'|'two-weeks'|'week'|'day'|'hour'|'minute'|'second';
+    this.every = JsonParserUtils.getString("every", schedule);
+    String dateStr = JsonParserUtils.getString("at", schedule);
+    if (dateStr != null) {
+      SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+      this.at = sdf.parse(dateStr);
+    }
+  }
+
+  public LocalNotificationSchedule() {
+  }
 
   public Date getAt() {
     return at;
@@ -18,12 +45,12 @@ public class LocalNotificationSchedule {
     this.at = at;
   }
 
-  public Boolean getRepeat() {
-    return repeat;
+  public Boolean getRepeats() {
+    return repeats;
   }
 
-  public void setRepeat(Boolean repeat) {
-    this.repeat = repeat;
+  public void setRepeats(Boolean repeats) {
+    this.repeats = repeats;
   }
 
   public String getEvery() {
@@ -34,11 +61,32 @@ public class LocalNotificationSchedule {
     this.every = every;
   }
 
-  public JSONObject getOn() {
-    return on;
+  public boolean isRepeating() {
+    return Boolean.TRUE.equals(this.repeats) || this.every != null;
   }
 
-  public void setOn(JSONObject on) {
-    this.on = on;
+  public Long getEveryInterval() {
+    switch (every) {
+      case "year":
+        return DateUtils.YEAR_IN_MILLIS;
+      case "month":
+        // This case is just approximation as months have different number of days
+        return 30 * DateUtils.DAY_IN_MILLIS;
+      case "two-weeks":
+        return 2 * DateUtils.WEEK_IN_MILLIS;
+      case "week":
+        return DateUtils.WEEK_IN_MILLIS;
+      case "day":
+        return DateUtils.DAY_IN_MILLIS;
+      case "hour":
+        return DateUtils.HOUR_IN_MILLIS;
+      case "minute":
+        return DateUtils.MINUTE_IN_MILLIS;
+      case "second":
+        return DateUtils.SECOND_IN_MILLIS;
+      default:
+        return null;
+    }
+
   }
 }
