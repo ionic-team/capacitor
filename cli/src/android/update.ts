ba0d609e7c58ecff0f1ab35e35500dc5a5f22b75
Julio César <jcesarmobile@gmail.com>
2018-07-10 21:21:10 +0200

    Add amazon schema to capacitor-android-plugins

--- cli/src/android/update.ts
+++ cli/src/android/update.ts
@@ -204,7 +204,9 @@ async function writeCordovaAndroidManifest(cordovaPlugins: Plugin[], config: Con
     });
   });
   let content = `<?xml version='1.0' encoding='utf-8'?>
-<manifest package="capacitor.android.plugins" xmlns:android='http://schemas.android.com/apk/res/android'>
+<manifest package="capacitor.android.plugins"
+xmlns:android="http://schemas.android.com/apk/res/android"
+xmlns:amazon="http://schemas.amazon.com/apk/res/android">
 <application>
 ${applicationXMLEntries.join('\n')}
 </application>
