0a99e1eb50da879454a1902bb5d34f594c9edbd6
jcesarmobile <jcesarmobile@gmail.com>
2018-08-08 19:11:27 +0200

    Add input file functionality to Android webview (#714)

--- android/capacitor/src/main/java/com/getcapacitor/BridgeWebChromeClient.java
+++ android/capacitor/src/main/java/com/getcapacitor/BridgeWebChromeClient.java
@@ -1,5 +1,7 @@
 package com.getcapacitor;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
 import android.webkit.ConsoleMessage;
@@ -11,12 +13,15 @@ import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
+import org.apache.cordova.CordovaPlugin;
+
 /**
  * Custom WebChromeClient handler, required for showing dialogs, confirms, etc. in our
  * WebView instance.
  */
 public class BridgeWebChromeClient extends WebChromeClient {
   private Bridge bridge;
+  static final int FILE_CHOOSER = PluginRequestCodes.FILE_CHOOSER;
 
   public BridgeWebChromeClient(Bridge bridge) {
     this.bridge = bridge;
@@ -134,8 +139,33 @@ public class BridgeWebChromeClient extends WebChromeClient {
   }
 
   @Override
-  public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
-    return super.onShowFileChooser(webView, filePathCallback, fileChooserParams);
+  public boolean onShowFileChooser(WebView webView, final ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+    Intent intent = fileChooserParams.createIntent();
+    if (fileChooserParams.getMode() == FileChooserParams.MODE_OPEN_MULTIPLE) {
+      intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+    }
+    try {
+      bridge.cordovaInterface.startActivityForResult(new CordovaPlugin() {
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+          Uri[] result;
+          if (intent.getClipData() != null && intent.getClipData().getItemCount() > 1) {
+            final int numFiles = intent.getClipData().getItemCount();
+            result = new Uri[numFiles];
+            for (int i = 0; i < numFiles; i++) {
+              result[i] = intent.getClipData().getItemAt(i).getUri();
+
+            }
+          } else {
+            result = WebChromeClient.FileChooserParams.parseResult(resultCode, intent);
+          }
+          filePathCallback.onReceiveValue(result);
+        }
+      }, intent, FILE_CHOOSER);
+    } catch (ActivityNotFoundException e) {
+      filePathCallback.onReceiveValue(null);
+    }
+    return true;
   }
 
   @Override
