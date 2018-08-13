e7ac5114c79c70f13540dd7df8631bbcd02bde2a
jcesarmobile <jcesarmobile@gmail.com>
2018-08-13 18:27:40 +0200

    WebView plugin implementation for iOS (#727)

---
+++ ios/Capacitor/Capacitor/Plugins/WebView.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+@objc(CAPWebViewPlugin)
+public class CAPWebViewPlugin : CAPPlugin {
+
+  @objc func setServerBasePath(_ call: CAPPluginCall) {
+    let path = call.getString("path")
+    let vc = bridge.viewController as! CAPBridgeViewController
+    vc.setServerBasePath(path: path!)
+    call.success()
+  }
+
+  @objc func getServerBasePath(_ call: CAPPluginCall) {
+    let vc = bridge.viewController as! CAPBridgeViewController
+    let path = vc.getServerBasePath()
+    call.success([
+      "path": path
+    ])
+  }
+}
