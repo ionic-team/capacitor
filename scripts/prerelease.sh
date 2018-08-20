0ecbd167294a7503fa2b87869a3f975fe3db6d42
Michael Oberwasserlechner <michael@byteowls.com>
2018-08-20 15:18:03 +0200

    refactor(gradle): change path to gradlew

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -5,5 +5,5 @@ pod spec lint --allow-warnings Capacitor.podspec
 pod spec lint --allow-warnings CapacitorCordova.podspec
 
 # Do the gradle
-cd android
-./gradlew clean build -b capacitor/build.gradle
+cd android/capacitor
+./gradlew clean build
