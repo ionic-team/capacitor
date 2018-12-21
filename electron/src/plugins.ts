92e9d21fab7671965581227020b86f8da19fdb89
jcesarmobile <jcesarmobile@gmail.com>
2018-12-21 15:58:04 +0100

    Remove unneeded imports

--- electron/src/plugins.ts
+++ electron/src/plugins.ts
@@ -1,16 +1,9 @@
 import { Plugins } from './global';
 import { mergeWebPlugins, mergeWebPlugin, WebPlugin } from '@capacitor/core';
 
-export * from '@capacitor/core/dist/esm/web/browser';
-export * from '@capacitor/core/dist/esm/web/camera';
-export * from '@capacitor/core/dist/esm/web/geolocation';
-export * from '@capacitor/core/dist/esm/web/device';
-export * from '@capacitor/core/dist/esm/web/share';
-export * from '@capacitor/core/dist/esm/web/storage';
 export * from './electron/filesystem';
 export * from './electron/network';
 export * from './electron/splashscreen';
-export * from '@capacitor/core/dist/esm/web/toast';
 export * from './electron/clipboard';
 export * from './electron/modals';
 
