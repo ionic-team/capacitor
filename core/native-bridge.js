c79a390b0ff141e1deba04fdf7757255b3172f7f
jcesarmobile <jcesarmobile@gmail.com>
2019-01-09 13:45:58 +0100

    Support converting file urls starting by / (#1112)

--- core/native-bridge.js
+++ core/native-bridge.js
@@ -506,6 +506,9 @@
     if (!url) {
       return url;
     }
+    if (url.startsWith('/')) {
+      return 'capacitor-file:///' + url;
+    }
     if (url.startsWith('file://')) {
       return url.replace('file', 'capacitor-file');
     }
