8f98c6ce90de093c702d4ed281f48d8c0b0511da
Max Lynch <max@drifty.com>
2019-01-26 10:42:24 -0600

    Publish: verify ANDROID_HOME is set

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -1,5 +1,7 @@
 set -e
 
+[ -z "$ANDROID_HOME" ] && echo "ANDROID_HOME is not set. Please set it before running this script." && exit 1;
+
 # Verify pods are good
 pod spec lint --allow-warnings Capacitor.podspec
 pod spec lint --allow-warnings CapacitorCordova.podspec
