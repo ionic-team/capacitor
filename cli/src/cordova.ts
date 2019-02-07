4935571542881975a5cdedab914ea9179344b743
jcesarmobile <jcesarmobile@gmail.com>
2019-02-07 18:22:57 +0100

    Add <access origin="*" /> to generated config.xml (#1206)

--- cli/src/cordova.ts
+++ cli/src/cordova.ts
@@ -151,6 +151,7 @@ export async function autoGenerateConfig(config: Config, cordovaPlugins: Plugin[
   }));
   const content = `<?xml version='1.0' encoding='utf-8'?>
   <widget version="1.0.0" xmlns="http://www.w3.org/ns/widgets" xmlns:cdv="http://cordova.apache.org/ns/1.0">
+  <access origin="*" />
   ${pluginEntriesString.join('\n')}
   </widget>`;
   await writeFileAsync(cordovaConfigXMLFile, content);
