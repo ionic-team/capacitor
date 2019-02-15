efc2cf8f056ca68f035b35422e729cff19b374e0
jcesarmobile <jcesarmobile@gmail.com>
2019-02-15 19:54:15 +0100

    Remove setTimeout from example and docs (#1229)

--- example/src/pages/background/background.ts
+++ example/src/pages/background/background.ts
@@ -30,16 +30,22 @@ export class BackgroundPage {
   startTask() {
     this.taskId = BackgroundTask.beforeExit(async () => {
       return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          console.log('TASK DONE!', this.taskId);
 
-          // Must call in order to end our task
-          BackgroundTask.finish({
-            taskId: this.taskId
-          });
+        // Do a long task you want to finish
+        // even if the device enters into background mode
+        var start = new Date().getTime();
+        for (var i = 0; i < 1e18; i++) {
+          if ((new Date().getTime() - start) > 20000){
+            break;
+          }
+        }
+        console.log('TASK DONE!', this.taskId);
 
-          resolve();
-        }, 30000);
+        // Must call in order to end our task
+        BackgroundTask.finish({
+          taskId: this.taskId
+        });
+        resolve();
       });
     })
     console.log('Starting background task:', this.taskId);
