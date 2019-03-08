41511d14bd343e30ffa9ea26aaa3d1e8419799ce
jcesarmobile <jcesarmobile@gmail.com>
2019-03-08 16:23:26 +0100

    Post a notification with the WKNavigationAction on decidePolicyForNavigationAction

--- ios/Capacitor/Capacitor/CAPNotifications.swift
+++ ios/Capacitor/Capacitor/CAPNotifications.swift
@@ -7,14 +7,16 @@
   case ContinueActivity
   case DidRegisterForRemoteNotificationsWithDeviceToken
   case DidFailToRegisterForRemoteNotificationsWithError
+  case DecidePolicyForNavigationAction
   
   public func name() -> String {
     switch self {
-    case .URLOpen: return "CAPNotificationsURLOpen"
-    case .UniversalLinkOpen: return "CAPUniversalLinkOpen"
-    case .ContinueActivity: return "CAPNotificationsContinueActivity"
-    case .DidRegisterForRemoteNotificationsWithDeviceToken: return "CAPDidRegisterForRemoteNotificationsWithDeviceToken"
-    case .DidFailToRegisterForRemoteNotificationsWithError: return "CAPDidFailToRegisterForRemoteNotificationsWithError"
+      case .URLOpen: return "CAPNotificationsURLOpen"
+      case .UniversalLinkOpen: return "CAPUniversalLinkOpen"
+      case .ContinueActivity: return "CAPNotificationsContinueActivity"
+      case .DidRegisterForRemoteNotificationsWithDeviceToken: return "CAPDidRegisterForRemoteNotificationsWithDeviceToken"
+      case .DidFailToRegisterForRemoteNotificationsWithError: return "CAPDidFailToRegisterForRemoteNotificationsWithError"
+      case .DecidePolicyForNavigationAction: return "CAPDecidePolicyForNavigationAction"
     }
   }
 }
