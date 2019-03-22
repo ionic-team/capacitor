a887657231436318a5341851b702e1aaec8faa60
jcesarmobile <jcesarmobile@gmail.com>
2019-03-22 18:54:01 +0100

    Update stencil on serve command

--- cli/src/web/serve.ts
+++ cli/src/web/serve.ts
@@ -3,6 +3,6 @@ import { Config } from '../config';
 
 export async function serveWeb(config: Config) {
   await runTask(`Serving web content in: ${config.app.webDir}`, () => {
-    return runCommand(`npx @stencil/core@0.18.1-3 serve --open --root ${config.app.webDir}`);
+    return runCommand(`npx @stencil/core@0.18.1 serve --open --root ${config.app.webDir}`);
   });
 }
