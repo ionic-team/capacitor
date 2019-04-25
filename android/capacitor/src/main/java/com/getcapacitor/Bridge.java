e15da8505892ca80c22b88287c417ab8ce9848b4
jcesarmobile <jcesarmobile@gmail.com>
2019-04-25 14:54:46 +0200

    Remove unused import

--- android/capacitor/src/main/java/com/getcapacitor/Bridge.java
+++ android/capacitor/src/main/java/com/getcapacitor/Bridge.java
@@ -9,7 +9,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
-import android.util.Base64;
 import android.util.Log;
 import android.webkit.ValueCallback;
 import android.webkit.WebResourceRequest;
