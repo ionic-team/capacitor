61c0d692c093d3cb1ab4132b1bc577716c319951
jcesarmobile <jcesarmobile@gmail.com>
2019-05-07 19:51:08 +0200

    Implementation of Device plugin for Electron

--- electron/src/plugins.ts
+++ electron/src/plugins.ts
@@ -1,11 +1,13 @@
 import { Plugins } from './global';
 import { mergeWebPlugins, mergeWebPlugin, WebPlugin } from '@capacitor/core';
 
+export * from './electron/clipboard';
+export * from './electron/device';
 export * from './electron/filesystem';
+export * from './electron/modals';
 export * from './electron/network';
 export * from './electron/splashscreen';
-export * from './electron/clipboard';
-export * from './electron/modals';
+
 
 mergeWebPlugins(Plugins);
 
