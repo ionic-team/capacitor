1cffe367698508b08e580ea9bad2609622e4f1f0
Simon Hänisch <simonhaenisch@users.noreply.github.com>
2019-05-27 13:57:51 +0200

    fix: make capacitor compatible with commonjs (#1575)
    * package.json browser and main entry updates

--- core/rollup.config.js
+++ core/rollup.config.js
@@ -1,14 +1,24 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 
+const banner = '/*! Capacitor: https://capacitor.ionicframework.com/ - MIT License */';
+
 export default {
   input: 'dist/esm/index.js',
-  output: {
-    file: 'dist/capacitor.js',
-    format: 'iife',
-    name: 'capacitorExports'
-  },
-  sourcemap: true,
-  banner: '/*! Capacitor: https://capacitor.ionicframework.com/ - MIT License */',
+  output: [
+    {
+      file: 'dist/capacitor.js',
+      format: 'iife',
+      name: 'capacitorExports',
+      banner,
+      sourcemap: true
+    },
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      banner,
+      sourcemap: true
+    }
+  ],
   plugins: [
     nodeResolve()
   ]
