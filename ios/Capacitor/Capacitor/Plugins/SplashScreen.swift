e0dcd4b8f9794af51bb071c8cf9c1dfdaeaa6488
Philipp Grosswiler <phil@Jessicas-MacBook-Air.local>
2019-06-14 12:07:58 +0700

    Added code formatting.

--- ios/Capacitor/Capacitor/Plugins/SplashScreen.swift
+++ ios/Capacitor/Capacitor/Plugins/SplashScreen.swift
@@ -2,7 +2,7 @@ import AudioToolbox
 import Foundation
 
 @objc(CAPSplashScreenPlugin)
-public class CAPSplashScreenPlugin : CAPPlugin {
+public class CAPSplashScreenPlugin: CAPPlugin {
   var imageView = UIImageView()
   var image: UIImage?
   var call: CAPPluginCall?
@@ -27,7 +27,9 @@ public class CAPSplashScreenPlugin : CAPPlugin {
     self.call = call
 
     if image == nil {
-      call.error("No image named \"Splash\" found. Please check your Assets.xcassets for a file named Splash")
+      call.error(
+        "No image named \"Splash\" found. Please check your Assets.xcassets for a file named Splash"
+      )
       return
     }
 
@@ -37,9 +39,17 @@ public class CAPSplashScreenPlugin : CAPPlugin {
     let autoHide = call.get("autoHide", Bool.self, defaultAutoHide)!
     let backgroundColor = call.get("backgroundColor", String.self)
 
-    showSplash(showDuration: showDuration, fadeInDuration: fadeInDuration, fadeOutDuration: fadeOutDuration, autoHide: autoHide, backgroundColor: backgroundColor, completion: {
-      call.success()
-    }, isLaunchSplash: false)
+    showSplash(
+      showDuration: showDuration,
+      fadeInDuration: fadeInDuration,
+      fadeOutDuration: fadeOutDuration,
+      autoHide: autoHide,
+      backgroundColor: backgroundColor,
+      completion: {
+        call.success()
+      },
+      isLaunchSplash: false
+    )
   }
 
   // Hide the splash screen
@@ -56,7 +66,9 @@ public class CAPSplashScreenPlugin : CAPPlugin {
     image = UIImage.init(named: "Splash")
 
     if image == nil {
-      print("Unable to find splash screen image. Make sure an image called Splash exists in your assets")
+      print(
+        "Unable to find splash screen image. Make sure an image called Splash exists in your assets"
+      )
     }
 
     // Observe for changes on fram and bounds to handle rotation resizing
@@ -77,12 +89,18 @@ public class CAPSplashScreenPlugin : CAPPlugin {
   // the parent view observers fire
   func updateSplashImageBounds() {
     guard let delegate = UIApplication.shared.delegate else {
-      bridge.modulePrint(self, "Unable to find root window object for SplashScreen bounds. Please file an issue")
+      bridge.modulePrint(
+        self,
+        "Unable to find root window object for SplashScreen bounds. Please file an issue"
+      )
       return
     }
 
     guard let window = delegate.window as? UIWindow else {
-      bridge.modulePrint(self, "Unable to find root window object for SplashScreen bounds. Please file an issue")
+      bridge.modulePrint(
+        self,
+        "Unable to find root window object for SplashScreen bounds. Please file an issue"
+      )
       return
     }
     imageView.image = image
@@ -90,21 +108,43 @@ public class CAPSplashScreenPlugin : CAPPlugin {
     imageView.contentMode = .scaleAspectFill
   }
 
-  public override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+  public override func observeValue(
+    forKeyPath keyPath: String?,
+    of object: Any?,
+    change: [NSKeyValueChangeKey: Any]?,
+    context: UnsafeMutableRawPointer?
+  ) {
     updateSplashImageBounds()
   }
 
   func showOnLaunch() {
     self.bridge.viewController.view.addSubview(self.imageView)
-    let launchShowDurationConfig = getConfigValue("launchShowDuration") as? Int ?? launchShowDuration
+    let launchShowDurationConfig = getConfigValue("launchShowDuration") as? Int
+      ?? launchShowDuration
     let launchAutoHideConfig = getConfigValue("launchAutoHide") as? Bool ?? launchAutoHide
     let launchBackgroundColorConfig = getConfigValue("backgroundColor") as? String ?? nil
 
-    showSplash(showDuration: launchShowDurationConfig, fadeInDuration: 0, fadeOutDuration: defaultFadeOutDuration, autoHide: launchAutoHideConfig, backgroundColor: launchBackgroundColorConfig, completion: {
-    }, isLaunchSplash: true)
+    showSplash(
+      showDuration: launchShowDurationConfig,
+      fadeInDuration: 0,
+      fadeOutDuration: defaultFadeOutDuration,
+      autoHide: launchAutoHideConfig,
+      backgroundColor: launchBackgroundColorConfig,
+      completion: {
+      },
+      isLaunchSplash: true
+    )
   }
 
-    func showSplash(showDuration: Int, fadeInDuration: Int, fadeOutDuration: Int, autoHide: Bool, backgroundColor: String?, completion: @escaping () -> Void, isLaunchSplash: Bool) {
+  func showSplash(
+    showDuration: Int,
+    fadeInDuration: Int,
+    fadeOutDuration: Int,
+    autoHide: Bool,
+    backgroundColor: String?,
+    completion: @escaping () -> Void,
+    isLaunchSplash: Bool
+  ) {
 
     DispatchQueue.main.async {
       if backgroundColor != nil {
@@ -117,36 +157,51 @@ public class CAPSplashScreenPlugin : CAPPlugin {
 
       self.bridge.viewController.view.isUserInteractionEnabled = false
 
-      UIView.transition(with: self.imageView, duration: TimeInterval(Double(fadeInDuration) / 1000), options: .curveLinear, animations: {
-        self.imageView.alpha = 1
-      }) { (finished: Bool) in
+      UIView.transition(
+        with: self.imageView,
+        duration: TimeInterval(Double(fadeInDuration) / 1000),
+        options: .curveLinear,
+        animations: {
+          self.imageView.alpha = 1
+        }
+      ) { (finished: Bool) in
         self.isVisible = true
 
         if autoHide {
-          self.hideTask = DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + (Double(showDuration) / 1000), execute: {
-            self.hideSplash(fadeOutDuration: fadeOutDuration, isLaunchSplash: isLaunchSplash)
-            completion()
-          })
+          self.hideTask = DispatchQueue.main.asyncAfter(
+            deadline: DispatchTime.now() + (Double(showDuration) / 1000),
+            execute: {
+              self.hideSplash(fadeOutDuration: fadeOutDuration, isLaunchSplash: isLaunchSplash)
+              completion()
+            }
+          )
         }
       }
     }
   }
 
   func hideSplash(fadeOutDuration: Int) {
-    self.hideSplash(fadeOutDuration: fadeOutDuration, isLaunchSplash: false);
+    self.hideSplash(fadeOutDuration: fadeOutDuration, isLaunchSplash: false)
   }
 
   func hideSplash(fadeOutDuration: Int, isLaunchSplash: Bool) {
-    if(isLaunchSplash && isVisible) {
-      print("SplashScreen.hideSplash: SplashScreen was automatically hidden after default timeout. " +
-            "You should call `SplashScreen.hide()` as soon as your web app is loaded (or increase the timeout). " +
-            "Read more at https://capacitor.ionicframework.com/docs/apis/splash-screen/#hiding-the-splash-screen");
+    if isLaunchSplash && isVisible {
+      print(
+        "SplashScreen.hideSplash: SplashScreen was automatically hidden after default timeout. "
+          + "You should call `SplashScreen.hide()` as soon as your web app is loaded (or increase the timeout). "
+          + "Read more at https://capacitor.ionicframework.com/docs/apis/splash-screen/#hiding-the-splash-screen"
+      )
     }
     if !isVisible { return }
     DispatchQueue.main.async {
-      UIView.transition(with: self.imageView, duration: TimeInterval(Double(fadeOutDuration) / 1000), options: .curveLinear, animations: {
-        self.imageView.alpha = 0
-      }) { (finished: Bool) in
+      UIView.transition(
+        with: self.imageView,
+        duration: TimeInterval(Double(fadeOutDuration) / 1000),
+        options: .curveLinear,
+        animations: {
+          self.imageView.alpha = 0
+        }
+      ) { (finished: Bool) in
         self.tearDown()
       }
     }
