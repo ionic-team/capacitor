49a4e55d492b38b887fd337e1443d9e99cfc1762
jcesarmobile <jcesarmobile@gmail.com>
2019-07-30 19:23:55 +0200

    fix(cli): remove deprecated dependencies

--- cli/src/util/archive.ts
+++
@@ -1,8 +0,0 @@
-import * as tarType from 'tar';
-
-export type TarExtractOptions = tarType.ExtractOptions & tarType.FileOptions;
-
-export async function createTarExtraction(opts?: TarExtractOptions): Promise<NodeJS.WritableStream> {
-  const tar = await import('tar');
-  return tar.extract(opts || {});
-}
