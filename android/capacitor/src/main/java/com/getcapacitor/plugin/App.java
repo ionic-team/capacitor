20c727f4116a8ad0032e52cf9d7ce324187fd015
Gustavo San Jose <gustavo.sanjose@coschedule.com>
2019-08-20 11:57:49 -0500

    [CHORE] Add missing import

--- android/capacitor/src/main/java/com/getcapacitor/plugin/App.java
+++ android/capacitor/src/main/java/com/getcapacitor/plugin/App.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Bundle;
 import android.util.Log;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
