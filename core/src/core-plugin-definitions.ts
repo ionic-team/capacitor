85df254bbb2e9d95192073822a7a39eea254fc63
Dylan Otruba <dylan.otruba@thrive.health>
2019-10-31 10:56:41 -0700

    add count to type definition of LocalNotificationSchedule, to work with every

--- core/src/core-plugin-definitions.ts
+++ core/src/core-plugin-definitions.ts
@@ -1032,6 +1032,7 @@ export interface LocalNotificationSchedule {
   at?: Date;
   repeats?: boolean;
   every?: 'year'|'month'|'two-weeks'|'week'|'day'|'hour'|'minute'|'second';
+  count?: number;
   on?: {
     year?: number;
     month?: number;
