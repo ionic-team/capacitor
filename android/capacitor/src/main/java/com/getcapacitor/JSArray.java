0cc877f6434ac5cb67e79f810045ba2869b3f891
Philipp Grosswiler <philipp.grosswiler@gmail.com>
2019-12-10 02:26:12 +0700

    chore(android): Add JSArray constructor to handle Collection types (#2233)

--- android/capacitor/src/main/java/com/getcapacitor/JSArray.java
+++ android/capacitor/src/main/java/com/getcapacitor/JSArray.java
@@ -4,12 +4,17 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 
 public class JSArray extends JSONArray {
   public JSArray() {
     super();
+  }
+
+  public JSArray(Collection copyFrom) {
+    super(copyFrom);
   }
 
   public JSArray(Object array) throws JSONException {
