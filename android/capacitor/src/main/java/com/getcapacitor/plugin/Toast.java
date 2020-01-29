35ece70d3937ba04e6a21ea66fed6d55217457be
Vincent Velociter <vincent.velociter@gmail.com>
2020-01-29 19:03:42 +0100

    Android default behavior

--- android/capacitor/src/main/java/com/getcapacitor/plugin/Toast.java
+++ android/capacitor/src/main/java/com/getcapacitor/plugin/Toast.java
@@ -11,9 +11,6 @@ public class Toast extends Plugin {
 
   private static final int GRAVITY_TOP = Gravity.TOP|Gravity.CENTER_HORIZONTAL;
   private static final int GRAVITY_CENTER = Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL;
-  private static final int GRAVITY_BOTTOM = Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL;
-
-  private static final int BASE_TOP_BOTTOM_OFFSET = 20;
 
   @PluginMethod()
   public void show(PluginCall call) {
@@ -30,20 +27,13 @@ public class Toast extends Plugin {
       duration = android.widget.Toast.LENGTH_LONG;
     }
 
-
     android.widget.Toast toast = android.widget.Toast.makeText(getContext(), text, duration);
 
     String position = call.getString("position", "bottom");
-
     if("top".equals(position)) {
-      toast.setGravity(GRAVITY_TOP, 0, BASE_TOP_BOTTOM_OFFSET);
-    } else  if("bottom".equals(position)) {
-      toast.setGravity(GRAVITY_BOTTOM, 0, BASE_TOP_BOTTOM_OFFSET);
+      toast.setGravity(GRAVITY_TOP, 0, 40);
     } else if("center".equals(position)) {
       toast.setGravity(GRAVITY_CENTER, 0, 0);
-    } else {
-      call.error("Invalid position. Valid options are 'top', 'center' and 'bottom'.");
-      return;
     }
 
     toast.show();
