9ba8e388724806c76ba347b0cd9dacd4e92f6b70
jcesarmobile <jcesarmobile@gmail.com>
2020-03-04 12:26:18 +0100

    chore: make deploy script publish Android before iOS

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,5 +26,5 @@ git push --follow-tags origin master
 #rm -rf capacitor-ios
 
 # Do the actual native deploys second, because they require tags/releases in github
-bash scripts/deploy/pods.sh
 bash scripts/deploy/android.sh
+bash scripts/deploy/pods.sh
