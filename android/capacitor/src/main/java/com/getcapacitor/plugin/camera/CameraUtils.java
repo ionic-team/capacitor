8b5d7d7ede088b9532d064897c7f600010f8013f
jcesarmobile <jcesarmobile@gmail.com>
2020-03-11 19:12:19 +0100

    fix(android): make camera work on Android 10

--- android/capacitor/src/main/java/com/getcapacitor/plugin/camera/CameraUtils.java
+++ android/capacitor/src/main/java/com/getcapacitor/plugin/camera/CameraUtils.java
@@ -16,22 +16,15 @@ import java.util.Date;
 
 public class CameraUtils {
     public static Uri createImageFileUri(Activity activity, String appId) throws IOException{
-        File photoFile = CameraUtils.createImageFile(activity, false);
+        File photoFile = CameraUtils.createImageFile(activity);
         return FileProvider.getUriForFile(activity, appId + ".fileprovider", photoFile);
     }
 
-    public static File createImageFile(Activity activity, boolean saveToGallery) throws IOException {
+    public static File createImageFile(Activity activity) throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir;
-        if(saveToGallery) {
-            Log.d(getLogTag(), "Trying to save image to public external directory");
-            storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-            storageDir.mkdirs();
-        }  else {
-            storageDir = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        }
+        File storageDir = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
 
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
