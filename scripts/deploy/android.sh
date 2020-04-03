a63e20374e0ebadff09114d8f4969eef58a417d5
jcesarmobile <jcesarmobile@gmail.com>
2020-04-03 20:52:26 +0200

    chore(android): fix release script to use Android X (#2687)

--- scripts/deploy/android.sh
+++ scripts/deploy/android.sh
@@ -6,4 +6,4 @@ cd android
 
 export BINTRAY_PKG_VERSION=$LERNA_VERSION
 
-./gradlew clean build -b capacitor/build.gradle bintrayUpload -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY -PdryRun=false
+./gradlew clean build -b capacitor/build.gradle  -Pandroid.useAndroidX=true -Pandroid.enableJetifier=true bintrayUpload -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY -PdryRun=false
