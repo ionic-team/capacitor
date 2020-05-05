3ca7ae2c35f97db360f74b9043d4e77e8108fe29
rafiek <r.mohamedjoesoef@hva.nl>
2020-05-05 09:57:35 +0200

    initial working toast

--- ios/Capacitor/Capacitor/CAPBridgeViewController.swift
+++ ios/Capacitor/Capacitor/CAPBridgeViewController.swift
@@ -171,6 +171,69 @@ public class CAPBridgeViewController: UIViewController, CAPBridgeDelegate, WKScr
     printLoadError()
     exit(1)
   }
+    
+  var toast: UILabel?
+    @objc func show(text: String) {
+    
+    let duration = 10000
+    let position = "bottom"
+    
+    DispatchQueue.main.async {
+      let vc = self.bridge!.viewController
+      
+      let maxSizeTitle : CGSize = CGSize(width: vc.view.bounds.size.width-32, height: vc.view.bounds.size.height)
+      
+      let lb = UILabel()
+      lb.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+      lb.textColor = UIColor.white
+      lb.textAlignment = .center
+      lb.text = text
+      lb.alpha = 0
+      lb.layer.cornerRadius = 18
+      lb.clipsToBounds  =  true
+      lb.lineBreakMode = .byWordWrapping
+      lb.numberOfLines = 0
+      
+      var expectedSizeTitle : CGSize = lb.sizeThatFits(maxSizeTitle)
+      // UILabel can return a size larger than the max size when the number of lines is 1
+      let minWidth = min(maxSizeTitle.width, expectedSizeTitle.width)
+      let minHeight = min(maxSizeTitle.height, expectedSizeTitle.height)
+      expectedSizeTitle = CGSize(width: minWidth, height: minHeight)
+        
+      let height = expectedSizeTitle.height+32
+      let y: CGFloat
+      if (position == "top") {
+        y = 40
+      } else if (position == "center") {
+        y = (vc.view.bounds.size.height/2) - (height/2)
+      } else {
+        y = vc.view.bounds.size.height - height - (height/2)
+      }
+
+      lb.frame = CGRect(
+        x: ((vc.view.bounds.size.width)/2) - ((expectedSizeTitle.width+32)/2),
+        y: y,
+        width: expectedSizeTitle.width+32,
+        height: height)
+      
+      lb.padding = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+      self.toast = lb
+      
+      vc.view.addSubview(lb)
+      
+      UIView.animateKeyframes(withDuration: 0.3, delay: 0, animations: {
+        self.toast!.alpha = 1.0
+      }, completion: {(isCompleted) in
+        
+        UIView.animate(withDuration: 0.3, delay: (Double(duration) / 1000), options: .curveEaseOut, animations: {
+          self.toast!.alpha = 0.0
+        }, completion: {(isCompleted) in
+          self.toast!.removeFromSuperview()
+        })
+
+      })
+    }
+  }
 
   func loadWebView() {
     let fullStartPath = URL(fileURLWithPath: assetsFolder).appendingPathComponent(startDir).appendingPathComponent("index")
@@ -180,6 +243,10 @@ public class CAPBridgeViewController: UIViewController, CAPBridgeDelegate, WKScr
 
     hostname = bridge!.config.getString("server.url") ?? "\(bridge!.getLocalUrl())"
     allowNavigationConfig = bridge!.config.getValue("server.allowNavigation") as? Array<String>
+
+    #if DEBUG
+      self.show(text:"Hallo")
+    #endif
 
 
     CAPLog.print("⚡️  Loading app at \(hostname!)...")
