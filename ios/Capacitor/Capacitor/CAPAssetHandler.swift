e064709d824e4e794ee0548b7dfeee5cf927516a
James Diacono <james@diacono.com.au>
2020-05-08 14:36:24 +1000

    fix(ios): Support CORS when using HMR

--- ios/Capacitor/Capacitor/CAPAssetHandler.swift
+++ ios/Capacitor/Capacitor/CAPAssetHandler.swift
@@ -24,29 +24,48 @@ class CAPAssetHandler: NSObject, WKURLSchemeHandler {
       let localUrl = URL.init(string: url.absoluteString)!
       let fileUrl = URL.init(fileURLWithPath: startPath)
 
+      // CORS support
+      let origin = urlSchemeTask.request.value(forHTTPHeaderField: "origin")
+      var headers =  origin != nil ? [
+        "Access-Control-Allow-Origin": origin!,
+        "Access-Control-Allow-Methods": "GET, OPTIONS",
+      ] : [String: String]()
+
       do {
-        var data = Data()
-        if !stringToLoad.contains("cordova.js") {
-          if isMediaExtension(pathExtension: url.pathExtension) {
-            data = try Data(contentsOf: fileUrl, options: Data.ReadingOptions.mappedIfSafe)
-          } else {
-            data = try Data(contentsOf: fileUrl)
+        switch urlSchemeTask.request.httpMethod {
+        case "GET":
+          var data = Data()
+          if !stringToLoad.contains("cordova.js") {
+            if isMediaExtension(pathExtension: url.pathExtension) {
+              data = try Data(contentsOf: fileUrl, options: Data.ReadingOptions.mappedIfSafe)
+            } else {
+              data = try Data(contentsOf: fileUrl)
+            }
           }
+          let mimeType = mimeTypeForExtension(pathExtension: url.pathExtension)
+          let expectedContentLength = data.count
+
+          headers["Content-Type"] = mimeType
+          headers["Cache-Control"] = "no-cache"
+
+          let urlResponse = URLResponse(url: localUrl, mimeType: mimeType, expectedContentLength: expectedContentLength, textEncodingName: nil)
+          let httpResponse = HTTPURLResponse(url: localUrl, statusCode: 200, httpVersion: nil, headerFields: headers)
+          if isMediaExtension(pathExtension: url.pathExtension) {
+              urlSchemeTask.didReceive(urlResponse)
+          } else {
+              urlSchemeTask.didReceive(httpResponse!)
+          }
+          urlSchemeTask.didReceive(data)
+        case "OPTIONS":
+          // CORS preflight
+          let response = HTTPURLResponse(url: localUrl, statusCode: 200, httpVersion: nil, headerFields: headers)
+          urlSchemeTask.didReceive(response!)
+
+        default:
+          // method not allowed
+          let response = HTTPURLResponse(url: localUrl, statusCode: 405, httpVersion: nil, headerFields: headers)
+          urlSchemeTask.didReceive(response!)
         }
-        let mimeType = mimeTypeForExtension(pathExtension: url.pathExtension)
-        let expectedContentLength = data.count
-        let headers =  [
-          "Content-Type": mimeType,
-          "Cache-Control": "no-cache"
-        ]
-        let urlResponse = URLResponse(url: localUrl, mimeType: mimeType, expectedContentLength: expectedContentLength, textEncodingName: nil)
-        let httpResponse = HTTPURLResponse(url: localUrl, statusCode: 200, httpVersion: nil, headerFields: headers)
-        if isMediaExtension(pathExtension: url.pathExtension) {
-            urlSchemeTask.didReceive(urlResponse)
-        } else {
-            urlSchemeTask.didReceive(httpResponse!)
-        }
-        urlSchemeTask.didReceive(data)
       } catch let error as NSError {
         urlSchemeTask.didFailWithError(error)
         return
