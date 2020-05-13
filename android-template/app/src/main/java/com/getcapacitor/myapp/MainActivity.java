20317184ea6eadea9f4668be185bc0e451a5584f
Steve Peters <stpeters@collegeboard.org>
2020-05-13 07:54:20 -0500

    In Android Studio (and Java in general), 4 space indentation is standard.

--- android-template/app/src/main/java/com/getcapacitor/myapp/MainActivity.java
+++ android-template/app/src/main/java/com/getcapacitor/myapp/MainActivity.java
@@ -8,14 +8,14 @@ import com.getcapacitor.Plugin;
 import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    // Initializes the Bridge
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      // Additional plugins you've installed go here
-      // Ex: add(TotallyAwesomePlugin.class);
-    }});
-  }
+        // Initializes the Bridge
+        this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+          // Additional plugins you've installed go here
+          // Ex: add(TotallyAwesomePlugin.class);
+        }});
+    }
 }
