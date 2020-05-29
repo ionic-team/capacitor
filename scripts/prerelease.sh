c407d749d127c5db292241458c5079f0d92c80b9
Carl Poole <mail@carlpoole.com>
2020-05-29 18:52:33 -0500

    chore: remove redundant checks for android_home and bintray (#3019)

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -1,12 +1,5 @@
 set -e
 
-[ -z "$ANDROID_HOME" ] && echo "ANDROID_HOME is not set. Please set it before running this script." && exit 1;
-
-if [ -z $BINTRAY_USER ]; then
-  echo "Must define BINTRAY_USER and BINTRAY_KEY env var before deploying. See https://github.com/ionic-team/capacitor/blob/master/.github/CONTRIBUTING.md"
-  exit 1
-fi
-
 # Verify pods are good
 pod lib lint --allow-warnings Capacitor.podspec
 pod lib lint --allow-warnings CapacitorCordova.podspec
@@ -14,4 +7,3 @@ pod lib lint --allow-warnings CapacitorCordova.podspec
 # Do the gradle
 cd android
 ./gradlew clean build -b capacitor/build.gradle -Pandroid.useAndroidX=true -Pandroid.enableJetifier=true
-
