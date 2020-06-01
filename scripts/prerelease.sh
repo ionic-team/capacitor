8be5de67a2f5d5dd5a14f766499f83ed8b912ddf
jcesarmobile <jcesarmobile@gmail.com>
2020-06-01 19:57:05 +0200

    chore: simplify ios publishing

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -1,9 +1,11 @@
 set -e
 
 # Verify pods are good
+cd ios
 pod lib lint --allow-warnings Capacitor.podspec
 pod lib lint --allow-warnings CapacitorCordova.podspec
 
+
 # Do the gradle
-cd android
+cd ../android
 ./gradlew clean build -b capacitor/build.gradle -Pandroid.useAndroidX=true -Pandroid.enableJetifier=true
