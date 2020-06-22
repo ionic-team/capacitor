da8b8a099685638f49c6379f9e03615a549a502a
Matt Netkow <matt.netkow@gmail.com>
2020-06-22 15:59:59 -0500

    docs(cli): clarify "missing web assets directory" error message (#3131)

--- cli/src/common.ts
+++ cli/src/common.ts
@@ -29,9 +29,10 @@ export async function checkWebDir(config: Config): Promise<string | null> {
   }
   if (!await existsAsync(config.app.webDirAbs)) {
     return `Capacitor could not find the web assets directory "${config.app.webDirAbs}".
-    Please create it, and make sure it has an index.html file. You can change
-    the path of this directory in capacitor.config.json.
-    More info: https://capacitor.ionicframework.com/docs/basics/configuring-your-app`;
+    Please create it and make sure it has an index.html file. You can change
+    the path of this directory in capacitor.config.json (webDir option).
+    You may need to compile the web assets for your app (typically 'npm run build').
+    More info: https://capacitor.ionicframework.com/docs/basics/building-your-app`;
   }
 
   if (!await existsAsync(join(config.app.webDirAbs, 'index.html'))) {
