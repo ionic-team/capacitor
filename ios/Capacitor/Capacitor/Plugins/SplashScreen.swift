4dd6904b151503f40092f7fa308a5268c3bdd8c4
chicho86 <sergio.milici@gmail.com>
2020-06-23 20:35:23 -0300

    fix(iOS): Splash.show not resolving promise

--- ios/Capacitor/Capacitor/Plugins/SplashScreen.swift
+++ ios/Capacitor/Capacitor/Plugins/SplashScreen.swift
@@ -183,6 +183,8 @@ public class CAPSplashScreenPlugin: CAPPlugin {
             self.hideSplash(fadeOutDuration: fadeOutDuration, isLaunchSplash: isLaunchSplash)
             completion()
           }
+        } else {
+            completion()
         }
       }
     }
