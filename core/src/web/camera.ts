e867dd0c3d14eb429df8651a3d0c9dd0924dfd47
Max Lynch <max@drifty.com>
2020-06-30 19:03:33 -0500

    Update core/src/web/camera.ts
    Co-authored-by: jcesarmobile <jcesarmobile@gmail.com>

--- core/src/web/camera.ts
+++ core/src/web/camera.ts
@@ -75,7 +75,7 @@ export class CameraPluginWeb extends WebPlugin implements CameraPlugin {
     }
 
     input.addEventListener('change', (_e: any) => {
-      console.log('CHANGE', _e);
+     
       const file = input.files[0];
       let format = 'jpeg';
       if (file.type === 'image/png') {
