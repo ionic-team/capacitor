a9a63e0063a311023c66ebdc8b11ec27e2b8767b
Daniel Imhoff <dwieeb@gmail.com>
2020-07-09 11:55:16 -0700

    fix whitespace

--- cli/src/tasks/new-plugin.ts
+++ cli/src/tasks/new-plugin.ts
@@ -167,24 +167,24 @@ async function createIosPlugin(config: Config, pluginPath: string, domain: strin
 }
 
 function generatePodspec(config: Config, answers: NewPluginAnswers) {
-  return `
-  require 'json'
+  return `require 'json'
 
-  package = JSON.parse(File.read(File.join(__dir__, 'package.json')))
+package = JSON.parse(File.read(File.join(__dir__, 'package.json')))
 
-  Pod::Spec.new do |s|
-    s.name = '${fixName(answers.name)}'
-    s.version = package['version']
-    s.summary = package['description']
-    s.license = package['license']
-    s.homepage = package['repository']['url']
-    s.author = package['author']
-    s.source = { :git => package['repository']['url'], :tag => s.version.to_s }
-    s.source_files = 'ios/Plugin/**/*.{swift,h,m,c,cc,mm,cpp}'
-    s.ios.deployment_target  = '${config.ios.minVersion}'
-    s.dependency 'Capacitor'
-    s.swift_version = '5.0'
-  end`;
+Pod::Spec.new do |s|
+  s.name = '${fixName(answers.name)}'
+  s.version = package['version']
+  s.summary = package['description']
+  s.license = package['license']
+  s.homepage = package['repository']['url']
+  s.author = package['author']
+  s.source = { :git => package['repository']['url'], :tag => s.version.to_s }
+  s.source_files = 'ios/Plugin/**/*.{swift,h,m,c,cc,mm,cpp}'
+  s.ios.deployment_target  = '${config.ios.minVersion}'
+  s.dependency 'Capacitor'
+  s.swift_version = '5.0'
+end
+`;
 }
 
 async function createAndroidPlugin(config: Config, pluginPath: string, domain: string, className: string) {
