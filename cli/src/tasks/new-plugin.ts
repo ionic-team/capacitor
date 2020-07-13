240df53ebef94b6057141d0cc647540862c5f9c0
Dan Imhoff <dwieeb@gmail.com>
2020-07-13 14:20:35 -0700

    Update cli/src/tasks/new-plugin.ts
    Co-authored-by: Ian Keith <iankeith@gmail.com>

--- cli/src/tasks/new-plugin.ts
+++ cli/src/tasks/new-plugin.ts
@@ -182,7 +182,7 @@ Pod::Spec.new do |s|
   s.source_files = 'ios/Plugin/**/*.{swift,h,m,c,cc,mm,cpp}'
   s.ios.deployment_target  = '${config.ios.minVersion}'
   s.dependency 'Capacitor'
-  s.swift_version = '5.0'
+  s.swift_version = '5.1'
 end
 `;
 }
