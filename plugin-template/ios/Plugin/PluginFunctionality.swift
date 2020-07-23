aac4fe31bf35ff12d8a2635830df4016f0a49f7f
jcesarmobile <jcesarmobile@gmail.com>
2020-07-23 14:07:19 +0200

    make function public

--- plugin-template/ios/Plugin/PluginFunctionality.swift
+++ plugin-template/ios/Plugin/PluginFunctionality.swift
@@ -2,7 +2,7 @@ import Foundation
 
 @objc public class CLASS_NAMEFunctionality: NSObject {
 // swiftlint:disable:previous type_name
-    @objc func echo(_ value: String) -> String {
+    @objc public func echo(_ value: String) -> String {
         return value
     }
 }
