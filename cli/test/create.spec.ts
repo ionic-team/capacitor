c29b361d180c52f108b2207c3daaac7c3dbf5134
Daniel Imhoff <dwieeb@gmail.com>
2020-08-11 17:12:47 -0700

    install platforms

--- cli/test/create.spec.ts
+++ cli/test/create.spec.ts
@@ -1,6 +1,13 @@
 import { join } from 'path';
 import { runCommand } from '../src/common';
-import { APP_ID, APP_NAME, run, mktmp, MappedFS } from './util';
+import {
+  APP_ID,
+  APP_NAME,
+  run,
+  mktmp,
+  MappedFS,
+  installPlatform,
+} from './util';
 
 describe('Create', () => {
   let appDirObj: any;
@@ -21,8 +28,10 @@ describe('Create', () => {
     await run(process.cwd(), `create "${appDir}" "${APP_NAME}" "${APP_ID}"`);
     expect(await FS.exists('capacitor.config.json')).toBe(true);
     await runCommand(`cd ${appDir} && npm install`);
+    await installPlatform(appDir, 'ios');
     await run(appDir, 'add ios');
     expect(await FS.exists('ios')).toBe(true);
+    await installPlatform(appDir, 'android');
     await run(appDir, 'add android');
     expect(await FS.exists('android')).toBe(true);
   });
