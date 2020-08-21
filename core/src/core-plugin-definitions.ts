77f6757e19113486b424916f2cd7d9a36391b264
jcesarmobile <jcesarmobile@gmail.com>
2020-08-21 21:16:15 +0200

    chore: remove Motion plugin (#3435)

--- core/src/core-plugin-definitions.ts
+++ core/src/core-plugin-definitions.ts
@@ -13,7 +13,6 @@ export interface PluginRegistry {
   Keyboard: KeyboardPlugin;
   LocalNotifications: LocalNotificationsPlugin;
   Modals: ModalsPlugin;
-  Motion: MotionPlugin;
   Network: NetworkPlugin;
   PushNotifications: PushNotificationsPlugin;
   Share: SharePlugin;
@@ -1316,60 +1315,6 @@ export interface ActionSheetOption {
 
 export interface ActionSheetResult {
   index: number;
-}
-
-//
-
-export interface MotionPlugin extends Plugin {
-  /**
-   * Listen for accelerometer data
-   */
-  addListener(
-    eventName: 'accel',
-    listenerFunc: (event: MotionEventResult) => void,
-  ): PluginListenerHandle;
-  /**
-   * Listen for device orientation change (compass heading, etc.)
-   */
-  addListener(
-    eventName: 'orientation',
-    listenerFunc: (event: MotionOrientationEventResult) => void,
-  ): PluginListenerHandle;
-
-  /**
-   * Remove all native listeners for this plugin
-   */
-  removeAllListeners(): void;
-}
-
-export type MotionWatchOrientationCallback = (
-  accel: MotionOrientationEventResult,
-) => void;
-export type MotionWatchAccelCallback = (accel: MotionEventResult) => void;
-
-export interface MotionOrientationEventResult {
-  alpha: number;
-  beta: number;
-  gamma: number;
-}
-
-export interface MotionEventResult {
-  acceleration: {
-    x: number;
-    y: number;
-    z: number;
-  };
-  accelerationIncludingGravity: {
-    x: number;
-    y: number;
-    z: number;
-  };
-  rotationRate: {
-    alpha: number;
-    beta: number;
-    gamma: number;
-  };
-  interval: number;
 }
 
 //
