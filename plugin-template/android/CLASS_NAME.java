136fbe8638119463421d916d87338eed1ba3171c
Daniel Imhoff <dwieeb@gmail.com>
2020-08-24 17:35:32 -0700

    remove plugin:generate

--- plugin-template/android/CLASS_NAME.java
+++
@@ -1,8 +0,0 @@
-package PACKAGE_NAME;
-
-public class CLASS_NAME {
-
-    public String echo(String value) {
-        return value;
-    }
-}
--- plugin-template/ios/Plugin/CLASS_NAME.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-@objc public class CLASS_NAME: NSObject {
-    // swiftlint:disable:previous type_name
-    @objc public func echo(_ value: String) -> String {
-        return value
-    }
-}
