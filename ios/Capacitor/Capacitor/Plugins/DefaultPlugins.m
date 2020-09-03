2a0883cf26674ef25597ee5c3b3a58e3ec5a9cd3
Ian Keith <iankeith@gmail.com>
2020-09-03 17:58:50 -0400

    Removing iOS implementation

--- ios/Capacitor/Capacitor/Plugins/DefaultPlugins.m
+++ ios/Capacitor/Capacitor/Plugins/DefaultPlugins.m
@@ -23,10 +23,6 @@ CAP_PLUGIN(CAPBrowserPlugin, "Browser",
   CAP_PLUGIN_METHOD(removeAllListeners, CAPPluginReturnNone);
 )
 
-CAP_PLUGIN(CAPCameraPlugin, "Camera",
-  CAP_PLUGIN_METHOD(getPhoto, CAPPluginReturnPromise);
-)
-
 CAP_PLUGIN(CAPConsolePlugin, "Console",
   CAP_PLUGIN_METHOD(log, CAPPluginReturnNone);
 )
