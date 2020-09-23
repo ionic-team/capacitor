3db6982b6f858570531fdb97cf9aa56bb4587cbe
Carl Poole <carl@ionic.io>
2020-09-23 13:55:44 -0500

    comment

--- android/capacitor/src/main/java/com/getcapacitor/Bridge.java
+++ android/capacitor/src/main/java/com/getcapacitor/Bridge.java
@@ -152,6 +152,7 @@ public class Bridge {
     this.config = new CapConfig(getActivity().getAssets(), config);
     Logger.init(this.config);
 
+    // Display splash screen if configured
     if (context instanceof BridgeActivity) {
       Splash.showOnLaunch((BridgeActivity) context, this.config);
     }
