f681278855246b87f89a86638f42af42daa1b93c
Felix Kirchengast <17876666+fkirc@users.noreply.github.com>
2020-09-24 05:56:48 +0200

    fix(android): BridgeFragment NullPointerException (#3553)

--- android/capacitor/src/main/java/com/getcapacitor/BridgeFragment.java
+++ android/capacitor/src/main/java/com/getcapacitor/BridgeFragment.java
@@ -153,7 +153,9 @@ public class BridgeFragment extends Fragment {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        this.bridge.onDestroy();
+        if (this.bridge != null) {
+            this.bridge.onDestroy();
+        }
         if (this.mockWebView != null) {
             mockWebView.handleDestroy();
         }
