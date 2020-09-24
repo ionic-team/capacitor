ec086b0df11b2fa66e7feecfdf87fab2dc81a4e3
Dan Imhoff <dwieeb@gmail.com>
2020-09-24 04:18:56 -0700

    fix(cli): halt update upon failure (#3595)

--- cli/test/update.android.spec.ts
+++ cli/test/update.android.spec.ts
@@ -1,5 +1,4 @@
-import { APP_ID, APP_NAME, CORDOVA_PLUGIN_ID, MappedFS, makeAppDir, makeConfig, run } from './util';
-import { updateCommand } from '../src/tasks/update';
+import { APP_ID, APP_NAME, CORDOVA_PLUGIN_ID, MappedFS, makeAppDir, run } from './util';
 
 describe.each([false, true])('Update: Android (monoRepoLike: %p)', (monoRepoLike) => {
   let appDirObj;
@@ -14,8 +13,6 @@ describe.each([false, true])('Update: Android (monoRepoLike: %p)', (monoRepoLike
     // Init in this directory so we can test add
     await run(appDir, `init "${APP_NAME}" "${APP_ID}" --npm-client npm`);
     await run(appDir, `add android`);
-    // Redundant, because add does this, but called explicitly for thoroughness
-    await updateCommand(makeConfig(appDir), 'android', false);
     FS = new MappedFS(appDir);
   });
 
