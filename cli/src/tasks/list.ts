68e872d3f845116ad0202dcd4e42673032b63092
Dan Imhoff <dwieeb@gmail.com>
2020-10-16 08:42:25 -0700

    feat(cli): add `capacitor run` command (#3599)

--- cli/src/tasks/list.ts
+++ cli/src/tasks/list.ts
@@ -33,9 +33,9 @@ export async function list(config: Config, platform: string): Promise<void> {
   const allPlugins = await getPlugins(config);
   let plugins: Plugin[] = [];
   if (platform === config.ios.name) {
-    plugins = getIOSPlugins(allPlugins);
+    plugins = await getIOSPlugins(allPlugins);
   } else if (platform === config.android.name) {
-    plugins = getAndroidPlugins(allPlugins);
+    plugins = await getAndroidPlugins(allPlugins);
   } else if (platform === config.web.name) {
     logger.info(`Listing plugins for ${c.input(platform)} is not possible.`);
     return;
