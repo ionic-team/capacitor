2a317f0f398a136453cc3a533261ab631bb4b881
Ian Keith <iankeith@gmail.com>
2020-10-22 13:49:16 -0400

    Revert "Reworked CAPLog to prevent race condition between it and CAPConfig."
    This reverts commit 28f1eed110f3795cd47e00efea7bf2e02074615e.

--- ios/Capacitor/Capacitor/CAPLog.swift
+++ ios/Capacitor/Capacitor/CAPLog.swift
@@ -1,26 +1,19 @@
 public class CAPLog {
-    public static var enableLogging: Bool = true
-    
-    private static var oneTimeConfigCheck: () -> () = {
-        // `dispatch_once` is not available since Swift 3. but, since static properties are implicitly lazy,
-        // this code will only execute once which is sufficient for our needs here. but since we need to do an
-        // async dispatch, there is a window of time where the default value will be valid before the config
-        // value(s) can be loaded.
-        DispatchQueue.main.async {
-            let config = CAPConfig()
-            if let configFlag = (config.getValue("ios.hideLogs") as? Bool) ?? (config.getValue("hideLogs") as? Bool) {
-                enableLogging = !configFlag
-            }
-        }
-        return {}
-    }()
-    
+
+    public static let config = CAPConfig()
+
     public static func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
-        oneTimeConfigCheck()
-        if enableLogging {
+        if !self.hideLogs() {
             for (itemIndex, item) in items.enumerated() {
                 Swift.print(item, terminator: itemIndex == items.count - 1 ? terminator : separator)
             }
         }
+    }
+
+    public static func hideLogs() -> Bool {
+        if let hideLogs = (config.getValue("ios.hideLogs") as? Bool) ?? (config.getValue("hideLogs") as? Bool) {
+            return hideLogs
+        }
+        return false
     }
 }
