3a48379e7ad46221494a1ccefdc91dfb201cefbc
devcafeine <stephane.balasse@devcafeine.fr>
2020-10-28 11:50:22 +0100

    Add import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale

--- android/capacitor/src/main/java/com/getcapacitor/plugin/Permissions.java
+++ android/capacitor/src/main/java/com/getcapacitor/plugin/Permissions.java
@@ -14,6 +14,7 @@ import com.getcapacitor.PluginMethod;
 import android.os.Build;
 
 import org.json.JSONArray;
+import static androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale;
 
 @NativePlugin()
 public class Permissions extends Plugin {
