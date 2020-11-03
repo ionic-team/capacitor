76264f27f6be0eba8f9886a61c8354d30cace188
Daniel Imhoff <dwieeb@gmail.com>
2020-11-03 08:35:54 -0800

    revert the help output

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -36,7 +36,7 @@ export async function run(): Promise<void> {
 
   program
     .command('init [appName] [appId]')
-    .description(`create a Capacitor configuration file`)
+    .description(`create a ${c.strong('capacitor.config.json')} file`)
     .option(
       '--web-dir <value>',
       'Optional: Directory of your projects built web assets',
