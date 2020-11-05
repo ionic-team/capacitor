8a0737e91946cef54dd3f29214a13235618347c8
Charlie <13204117850@163.com>
2020-11-05 11:03:26 +0800

    Update CAPLog.swift
    Security issues, make code more robust

--- ios/Capacitor/Capacitor/CAPLog.swift
+++ ios/Capacitor/Capacitor/CAPLog.swift
@@ -5,7 +5,9 @@ public class CAPLog {
     public static func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
         if !self.hideLogs() {
             for (itemIndex, item) in items.enumerated() {
+                #if DEBUG
                 Swift.print(item, terminator: itemIndex == items.count - 1 ? terminator : separator)
+                #endif
             }
         }
     }
