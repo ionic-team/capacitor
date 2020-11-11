5f53de020aa42f5c206d875fea274097650f94ca
Daniel Imhoff <dwieeb@gmail.com>
2020-11-11 14:24:01 -0800

    add notification delegate

---
+++ ios/Capacitor/Capacitor/UserNotificationCenterDelegate.swift
@@ -0,0 +1,54 @@
+import Foundation
+
+@objc(CAPUNUserNotificationCenterDelegate) public class UserNotificationCenterDelegate: NSObject, UNUserNotificationCenterDelegate  {
+    public override init() {
+        // TODO: make better
+        super.init()
+        let center = UNUserNotificationCenter.current()
+        if center.delegate == nil {
+            center.delegate = self
+        }
+    }
+    
+    public weak var pushNotificationHandler: NotificationHandlerProtocol? {
+        didSet {
+            if pushNotificationHandler != nil, oldValue != nil {
+                CAPLog.print("Push notification handler overriding previous instance: \(String(describing: type(of: oldValue)))")
+            }
+        }
+    }
+    
+    public weak var localNotificationHandler: NotificationHandlerProtocol? {
+        didSet {
+            if localNotificationHandler != nil, oldValue != nil {
+                CAPLog.print("Local notification handler overriding previous instance: \(String(describing: type(of: oldValue)))")
+            }
+        }
+    }
+
+    public func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                       willPresent notification: UNNotification,
+                                       withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let presentationOptions: UNNotificationPresentationOptions?
+
+        if notification.request.trigger?.isKind(of: UNPushNotificationTrigger.self) == true {
+            presentationOptions = pushNotificationHandler?.willPresent(notification: notification)
+        } else {
+            presentationOptions = localNotificationHandler?.willPresent(notification: notification)
+        }
+        
+        completionHandler(presentationOptions ?? [])
+    }
+
+    public func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                       didReceive response: UNNotificationResponse,
+                                       withCompletionHandler completionHandler: @escaping () -> Void) {
+        if response.notification.request.trigger?.isKind(of: UNPushNotificationTrigger.self) == true {
+            pushNotificationHandler?.didReceive(response: response)
+        } else {
+            localNotificationHandler?.didReceive(response: response)
+        }
+        
+        completionHandler()
+    }
+}
