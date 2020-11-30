5b5933cd3bdfef42461099bfba55e4ee05da8071
Dan Imhoff <dwieeb@gmail.com>
2020-11-30 15:26:11 -0600

    chore(scripts): add version hook for syncing peer deps (#3877)

---
+++ scripts/lib/version.mjs
@@ -0,0 +1,15 @@
+import { readJSON, writeJSON } from './fs.mjs';
+
+export const setPackageJsonDependencies = async (
+  path,
+  packages,
+  key = 'dependencies',
+) => {
+  const pkg = await readJSON(path);
+
+  for (const [dep, version] of Object.entries(packages)) {
+    pkg[key][dep] = version;
+  }
+
+  await writeJSON(path, pkg);
+};
