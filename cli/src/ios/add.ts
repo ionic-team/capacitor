455c4304ad4a255528050d33b0d7dc7405cee0d9
Daniel Imhoff <dwieeb@gmail.com>
2020-12-08 13:46:54 -0800

    extract assets in cli

--- cli/src/ios/add.ts
+++ cli/src/ios/add.ts
@@ -1,14 +1,14 @@
 import c from '../colors';
 import { runTask } from '../common';
 import type { Config } from '../definitions';
-import { copyTemplate } from '../util/template';
+import { extractTemplate } from '../util/template';
 
 export async function addIOS(config: Config): Promise<void> {
   await runTask(
     `Adding native Xcode project in ${c.strong(config.ios.platformDir)}`,
     () => {
-      return copyTemplate(
-        config.ios.assets.templateDir,
+      return extractTemplate(
+        config.cli.assets.ios.platformTemplateArchiveAbs,
         config.ios.platformDirAbs,
       );
     },
