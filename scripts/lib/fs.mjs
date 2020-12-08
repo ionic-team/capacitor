7b29a7f1509a096fd0bbe4a44b661a714b84329d
Daniel Imhoff <dwieeb@gmail.com>
2020-12-08 14:03:04 -0800

    ensure assets dir exists

--- scripts/lib/fs.mjs
+++ scripts/lib/fs.mjs
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import * as util from 'util';
 
+export const mkdir = util.promisify(fs.mkdir);
 export const readFile = util.promisify(fs.readFile);
 export const readJSON = async p => JSON.parse(await readFile(p));
 export const writeFile = util.promisify(fs.writeFile);
--- scripts/pack-cli-assets.mjs
+++ scripts/pack-cli-assets.mjs
@@ -2,10 +2,13 @@ import { resolve } from 'path';
 import tar from 'tar';
 
 import { execute } from './lib/cli.mjs';
+import { mkdir } from './lib/fs.mjs';
 import { lsfiles } from './lib/git.mjs';
 import { root } from './lib/repo.mjs';
 
 execute(async () => {
+  const assetsDir = resolve(root, 'cli', 'assets');
+
   const templates = [
     'android-template',
     'ios-template',
@@ -13,8 +16,10 @@ execute(async () => {
     'capacitor-cordova-ios-plugins',
   ];
 
+  await mkdir(assetsDir, { recursive: true });
+
   for (const template of templates) {
-    const dest = resolve(root, 'cli', 'assets', template + '.tar.gz');
+    const dest = resolve(assetsDir, template + '.tar.gz');
     const templatePath = resolve(root, template);
 
     const files = await lsfiles(templatePath, { cwd: templatePath });
