19a74f816434e928d894b16c82f7580aa506c645
Ian Keith <iankeith@gmail.com>
2020-12-21 08:35:13 -0500

    Documentation

--- ios/Capacitor/Capacitor/CAPInstanceDescriptor.h
+++ ios/Capacitor/Capacitor/CAPInstanceDescriptor.h
@@ -43,7 +43,19 @@ NS_SWIFT_NAME(InstanceDescriptor)
 @property (nonatomic, readonly) CAPInstanceType instanceType;
 @property (nonatomic, retain, nonnull) NSDictionary *legacyConfig;
 
+/**
+ Initialize the descriptor with the default environment. This assumes that the application was built with the help of the Capacitor CLI
+ and that that the web app is located inside the application bundle at `public/`.
+ */
 - (instancetype _Nonnull)initAsDefault NS_SWIFT_NAME(init());
+
+/**
+ Initialize the descriptor for use in other contexts. The app location is the one required parameter.
+ 
+ @param appURL The location of the folder containing the web app.
+ @param configURL The location of the Capacitor configuration file.
+ @param cordovaURL The location of the Cordova configuration file.
+ */
 - (instancetype _Nonnull)initAtLocation:(NSURL* _Nonnull)appURL configuration:(NSURL* _Nullable)configURL cordovaConfiguration:(NSURL* _Nullable)cordovaURL NS_SWIFT_NAME(init(at:configuration:cordovaConfiguration:));
 @end
 
