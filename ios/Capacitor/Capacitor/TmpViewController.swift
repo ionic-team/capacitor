838ad582cd67c8613257bc765dc97d9b143c919a
jcesarmobile <jcesarmobile@gmail.com>
2020-12-21 17:05:23 +0100

    chore: Bump minimum iOS version to 12

--- ios/Capacitor/Capacitor/TmpViewController.swift
+++ ios/Capacitor/Capacitor/TmpViewController.swift
@@ -4,7 +4,7 @@ internal class TmpViewController: UIViewController {
     var count = 0
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        // On iOS 11-12 viewDidAppear is called when TmpViewController is created and
+        // On iOS 12 viewDidAppear is called when TmpViewController is created and
         // when the presented VC gets dismissed.
         // On iOS 13 only when the presented VC gets dismissed.
         // We only want to send the notification when the presented VC gets dismissed.
