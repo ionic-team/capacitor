059f4e583bca800b2590fcd9437721bc7a2fb53a
Joseph Pender <joey@prodcode.net>
2021-01-11 17:32:44 -0600

    Simplifying getDate

--- ios/Capacitor/Capacitor/JSTypes.swift
+++ ios/Capacitor/Capacitor/JSTypes.swift
@@ -149,14 +149,7 @@ extension JSValueContainer {
     }
 
     public func getDate(_ key: String) -> Date? {
-        if let isoString = jsObjectRepresentation[key] as? String {
-            return Self.jsDateFormatter.date(from: isoString)
-        }
-        
-        if let date = jsObjectRepresentation[key] as? NSDate {
-            return date as Date
-        }
-        return nil
+        return jsObjectRepresentation[key] as? Date        
     }
 
     public func getArray(_ key: String) -> JSArray? {
