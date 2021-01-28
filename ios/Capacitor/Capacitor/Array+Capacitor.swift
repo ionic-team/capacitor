64af6dca8f2ed29a595ef33b53cc50dbc9537c90
Carl Poole <carl@ionic.io>
2021-01-28 10:57:11 -0600

    Fixing unintended change to a swift file

--- ios/Capacitor/Capacitor/Array+Capacitor.swift
+++ ios/Capacitor/Capacitor/Array+Capacitor.swift
@@ -1,7 +1,7 @@
 // convenience wrappers to transform Arrays between NSNull and Optional values, for interoperability with Obj-C
 extension Array: CapacitorExtension {}
-extension CapacitorExtensionTypeWrapper where T == [JSValue] {
-    public func replacingNullValues() -> [JSValue?] {
+extension CapacitorExtensionTypeWrapper where T == Array<JSValue> {
+    public func replacingNullValues() -> Array<JSValue?> {
         return baseType.map({ (value) -> JSValue? in
             if value is NSNull {
                 return nil
@@ -9,18 +9,18 @@ extension CapacitorExtensionTypeWrapper where T == [JSValue] {
             return value
         })
     }
-
-    public func replacingOptionalValues() -> [JSValue] {
+    
+    public func replacingOptionalValues() -> Array<JSValue> {
         return baseType
     }
 }
 
-extension CapacitorExtensionTypeWrapper where T == [JSValue?] {
-    public func replacingNullValues() -> [JSValue?] {
+extension CapacitorExtensionTypeWrapper where T == Array<JSValue?> {
+    public func replacingNullValues() -> Array<JSValue?> {
         return baseType
     }
-
-    public func replacingOptionalValues() -> [JSValue] {
+    
+    public func replacingOptionalValues() -> Array<JSValue> {
         return baseType.map({ (value) -> JSValue in
             if let value = value {
                 return value
