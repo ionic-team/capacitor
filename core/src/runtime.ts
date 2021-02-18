ebf66bc8878927d4ba897436d837fbb531b55491
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2021-02-18 08:12:59 -0500

    fix: dont overwrite cap props using custom plat

--- core/src/runtime.ts
+++ core/src/runtime.ts
@@ -239,23 +239,25 @@ export const createCapacitor = (win: WindowCapacitor): CapacitorInstance => {
 
     return proxy;
   };
+  
+  if (!(cap as any).CUSTOM_PLATFORM) {
+    cap.convertFileSrc = convertFileSrc;
+    cap.getPlatform = getPlatform;
+    cap.getServerUrl = () => webviewServerUrl;
+    cap.handleError = handleError;
+    cap.isNativePlatform = isNativePlatform;
+    cap.isPluginAvailable = isPluginAvailable;
+    cap.logJs = logJs;
+    cap.pluginMethodNoop = pluginMethodNoop;
+    cap.registerPlugin = registerPlugin;
+    cap.Exception = CapacitorException;
+    cap.DEBUG = !!cap.DEBUG;
 
-  cap.convertFileSrc = convertFileSrc;
-  cap.getPlatform = getPlatform;
-  cap.getServerUrl = () => webviewServerUrl;
-  cap.handleError = handleError;
-  cap.isNativePlatform = isNativePlatform;
-  cap.isPluginAvailable = isPluginAvailable;
-  cap.logJs = logJs;
-  cap.pluginMethodNoop = pluginMethodNoop;
-  cap.registerPlugin = registerPlugin;
-  cap.Exception = CapacitorException;
-  cap.DEBUG = !!cap.DEBUG;
-
-  initBridge(win, cap);
-  initEvents(win, cap);
-  initVendor(win, cap);
-  initLegacyHandlers(win, cap);
+    initBridge(win, cap);
+    initEvents(win, cap);
+    initVendor(win, cap);
+    initLegacyHandlers(win, cap);
+  }
 
   return cap;
 };
