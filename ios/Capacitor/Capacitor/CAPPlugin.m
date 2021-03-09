d45ae1c0c65a844361da2aab29f5f18f5278cf6f
Ian Keith <iankeith@gmail.com>
2021-03-08 19:14:03 -0500

    Deprecating convenience accessors on CAPPlugin, falling through to the implementation on the call.

--- ios/Capacitor/Capacitor/CAPPlugin.m
+++ ios/Capacitor/Capacitor/CAPPlugin.m
@@ -1,4 +1,5 @@
 #import "CAPPlugin.h"
+#import "CAPBridgedJSTypes.h"
 #import <Capacitor/Capacitor-Swift.h>
 #import <Foundation/Foundation.h>
 
@@ -19,34 +20,13 @@
   return self.pluginName;
 }
 
--(BOOL) getBool:(CAPPluginCall *)call field:(NSString *)field defaultValue:(BOOL)defaultValue {
-  id idVal = [call.options objectForKey:field];
-  
-  if(![idVal isKindOfClass:[NSNumber class]]) {
-    return defaultValue;
-  }
-  
-  NSNumber *value = (NSNumber *)idVal;
-  if(value == nil) {
-    return defaultValue;
-  }
-  if(value.integerValue == 0) {
-    return FALSE;
-  }
-  return TRUE;
+- (BOOL)getBool:(CAPPluginCall *)call field:(NSString *)field defaultValue:(BOOL)defaultValue {
+  NSNumber* value = [call getNumber:field defaultValue:[NSNumber numberWithBool:defaultValue]];
+  return [value boolValue];
 }
 
--(NSString *) getString:(CAPPluginCall *)call field:(NSString *)field defaultValue:(NSString *)defaultValue
-{
-  id idVal = [call.options objectForKey:field];
-  if(![idVal isKindOfClass:[NSString class]]) {
-    return defaultValue;
-  }
-  NSString *value = (NSString *)idVal;
-  if(value == nil) {
-    return defaultValue;
-  }
-  return value;
+- (NSString *) getString:(CAPPluginCall *)call field:(NSString *)field defaultValue:(NSString *)defaultValue {
+  return [call getString:field defaultValue:defaultValue];
 }
 
 -(id)getConfigValue:(NSString *)key {
