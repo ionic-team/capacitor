ca7da97aff9ea98d2cf873087266cd87d52ff32b
Thomas Vidas <hello@thomasvidas.com>
2021-04-27 12:43:51 -0400

    Use duplicated platformId code so exceptions don't get mismatched

--- ios/Capacitor/Capacitor/assets/native-bridge.js
+++ ios/Capacitor/Capacitor/assets/native-bridge.js
@@ -5,41 +5,22 @@
 const nativeBridge = (function (exports) {
     'use strict';
 
-    var ExceptionCode;
-    (function (ExceptionCode) {
-        /**
-         * API is not implemented.
-         *
-         * This usually means the API can't be used because it is not implemented for
-         * the current platform.
-         */
-        ExceptionCode["Unimplemented"] = "UNIMPLEMENTED";
-        /**
-         * API is not available.
-         *
-         * This means the API can't be used right now because:
-         *   - it is currently missing a prerequisite, such as network connectivity
-         *   - it requires a particular platform or browser version
-         */
-        ExceptionCode["Unavailable"] = "UNAVAILABLE";
-    })(ExceptionCode || (ExceptionCode = {}));
-    const getPlatformId = (win) => {
-        var _a, _b;
-        if (win === null || win === void 0 ? void 0 : win.androidBridge) {
-            return 'android';
-        }
-        else if ((_b = (_a = win === null || win === void 0 ? void 0 : win.webkit) === null || _a === void 0 ? void 0 : _a.messageHandlers) === null || _b === void 0 ? void 0 : _b.bridge) {
-            return 'ios';
-        }
-        else {
-            return 'web';
-        }
-    };
-
     // For removing exports for iOS/Android, keep let for reassignment
     // eslint-disable-next-line
     let dummy = {};
     const initBridge = (w) => {
+        const getPlatformId = (win) => {
+            var _a, _b;
+            if (win === null || win === void 0 ? void 0 : win.androidBridge) {
+                return 'android';
+            }
+            else if ((_b = (_a = win === null || win === void 0 ? void 0 : win.webkit) === null || _a === void 0 ? void 0 : _a.messageHandlers) === null || _b === void 0 ? void 0 : _b.bridge) {
+                return 'ios';
+            }
+            else {
+                return 'web';
+            }
+        };
         const convertFileSrcServerUrl = (webviewServerUrl, filePath) => {
             if (typeof filePath === 'string') {
                 if (filePath.startsWith('/')) {
