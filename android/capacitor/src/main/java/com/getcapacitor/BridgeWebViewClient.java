6a7c4e3b3a250270ac5c4b0f09da2a613ef2cf17
Ely Lucas <ely@meta-tek.net>
2021-07-19 09:27:28 -0600

    fix(android): add missing android webview lifecycle events
    Co-authored-by: Christian Davatz <crixx@davatz.eu>
    Co-authored-by: Carl Poole <mail@carlpoole.com>

--- android/capacitor/src/main/java/com/getcapacitor/BridgeWebViewClient.java
+++ android/capacitor/src/main/java/com/getcapacitor/BridgeWebViewClient.java
@@ -2,6 +2,7 @@ package com.getcapacitor;
 
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
@@ -35,7 +36,6 @@ public class BridgeWebViewClient extends WebViewClient {
     @Override
     public void onPageFinished(WebView view, String url) {
         super.onPageFinished(view, url);
-
         List<WebViewListener> webViewListeners = bridge.getWebViewListeners();
 
         if (webViewListeners != null && view.getProgress() == 100) {
@@ -46,8 +46,39 @@ public class BridgeWebViewClient extends WebViewClient {
     }
 
     @Override
+    public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+        super.onReceivedError(view, request, error);
+
+        List<WebViewListener> webViewListeners = bridge.getWebViewListeners();
+        if (webViewListeners != null) {
+            for (WebViewListener listener : bridge.getWebViewListeners()) {
+                listener.onReceivedError(view);
+            }
+        }
+    }
+
+    @Override
     public void onPageStarted(WebView view, String url, Bitmap favicon) {
         super.onPageStarted(view, url, favicon);
         bridge.reset();
+        List<WebViewListener> webViewListeners = bridge.getWebViewListeners();
+
+        if (webViewListeners != null) {
+            for (WebViewListener listener : bridge.getWebViewListeners()) {
+                listener.onPageStarted(view);
+            }
+        }
+    }
+
+    @Override
+    public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
+        super.onReceivedHttpError(view, request, errorResponse);
+
+        List<WebViewListener> webViewListeners = bridge.getWebViewListeners();
+        if (webViewListeners != null) {
+            for (WebViewListener listener : bridge.getWebViewListeners()) {
+                listener.onReceivedHttpError(view);
+            }
+        }
     }
 }
