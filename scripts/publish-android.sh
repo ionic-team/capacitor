81e54a097c6e1e0946156fd92cd4ab3e19a2b6c5
Carl Poole <carl@ionic.io>
2021-08-24 16:44:06 -0500

    script updates and github ci

--- scripts/publish-android.sh
+++ scripts/publish-android.sh
@@ -1,33 +1,39 @@
 #!/usr/bin/env bash
 
- DIR=../android
- LOG_OUTPUT=./tmp/capacitor-android.txt
- CAP_VERSION=`grep '"version": ' $DIR/package.json | awk '{print $2}' | tr -d '",'`
- echo Attempting to build and publish Capacitor native libraries with version $CAP_VERSION
+DIR=../android
+LOG_OUTPUT=./tmp/capacitor-android.txt
+CAP_VERSION=`grep '"version": ' $DIR/package.json | awk '{print $2}' | tr -d '",'`
+echo Attempting to build and publish Capacitor native libraries with version $CAP_VERSION
 
- # Make log dir if doesnt exist
- mkdir -p ./tmp
+# Make log dir if doesnt exist
+mkdir -p ./tmp
 
- # Export ENV variable used by Gradle for Versioning
- export CAP_VERSION
+# Export ENV variable used by Gradle for Versioning
+export CAP_VERSION
 
- # Build and publish
- $DIR/gradlew clean build publishReleasePublicationToSonatypeRepository -b $DIR/capacitor/build.gradle -Pandroid.useAndroidX=true -Pandroid.enableJetifier=true > $LOG_OUTPUT 2>&1
+# Get latest com.capacitorjs:core XML version info
+CAPACITOR_PUBLISHED_URL="https://repo1.maven.org/maven2/com/capacitorjs/core/maven-metadata.xml"
+CAPACITOR_PUBLISHED_DATA=$(curl -s $CAPACITOR_PUBLISHED_URL)
+CAPACITOR_PUBLISHED_VERSION="$(perl -ne 'print and last if s/.*<latest>(.*)<\/latest>.*/\1/;' <<< $CAPACITOR_PUBLISHED_DATA)"
 
+CAPACITOR_PUBLISHED_VERSION="3.1.2"
 
-#  # Build and publish
-#  $DIR/gradlew clean build publishCapacitorPublicationToGithubPackagesRepository -b $DIR/capacitor/build.gradle -Pandroid.useAndroidX=true -Pandroid.enableJetifier=true > $LOG_OUTPUT 2>&1
+# Check if we need to publish a new native version of the Capacitor Android library
+if [[ $CAP_VERSION == $CAPACITOR_PUBLISHED_VERSION ]]; then
+    printf %"s\n" "Native Capacitor Android library version $CAPACITOR_PUBLISHED_VERSION is already published on MavenCentral, skipping."
+else
+    printf %"s\n" "Latest native Capacitor Android library is version $CAPACITOR_PUBLISHED_VERSION, publishing to MavenCentral staging..."
 
-#  echo $RESULT 
+    # Build and publish
+    $DIR/gradlew clean build publishReleasePublicationToSonatypeRepository --max-workers 1 -b $DIR/capacitor/build.gradle -Pandroid.useAndroidX=true -Pandroid.enableJetifier=true > $LOG_OUTPUT 2>&1
 
-#  if grep --quiet "Conflict" $LOG_OUTPUT; then
-#      printf %"s\n\n" "Duplicate: a published capacitor exists for version $CAP_VERSION, skipping."
-#  else
-#      if grep --quiet "BUILD SUCCESSFUL" $LOG_OUTPUT; then
-#          printf %"s\n\n" "Success: capacitor version $CAP_VERSION published."
-#      else
-#          printf %"s\n\n" "Error publishing, check $LOG_OUTPUT for more info!"
-#          cat $LOG_OUTPUT
-#          exit 1
-#      fi
-#  fi
+    echo $RESULT
+
+    if grep --quiet "BUILD SUCCESSFUL" $LOG_OUTPUT; then
+        printf %"s\n" "Success: Capacitor Android Library published to MavenCentral Staging. Manually review and release from the Sonatype Repository Manager https://s01.oss.sonatype.org/"
+    else
+        printf %"s\n" "Error publishing, check $LOG_OUTPUT for more info!"
+        cat $LOG_OUTPUT
+        exit 1
+    fi
+fi
