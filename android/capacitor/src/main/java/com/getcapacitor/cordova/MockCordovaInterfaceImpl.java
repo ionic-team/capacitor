669706b91abad7bc295e559b26351f30e05a496b
jcesarmobile <jcesarmobile@gmail.com>
2021-10-06 15:40:45 +0200

    fix(android): Support cordova-android 10

--- android/capacitor/src/main/java/com/getcapacitor/cordova/MockCordovaInterfaceImpl.java
+++ android/capacitor/src/main/java/com/getcapacitor/cordova/MockCordovaInterfaceImpl.java
@@ -1,7 +1,8 @@
 package com.getcapacitor.cordova;
 
-import android.app.Activity;
 import android.util.Pair;
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.concurrent.Executors;
 import org.apache.cordova.CordovaInterfaceImpl;
 import org.apache.cordova.CordovaPlugin;
@@ -9,7 +10,7 @@ import org.json.JSONException;
 
 public class MockCordovaInterfaceImpl extends CordovaInterfaceImpl {
 
-    public MockCordovaInterfaceImpl(Activity activity) {
+    public MockCordovaInterfaceImpl(AppCompatActivity activity) {
         super(activity, Executors.newCachedThreadPool());
     }
 
