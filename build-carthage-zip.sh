1d8eacedfe9fe1cdbc8af3cdd78c6f13c5d20745
Thomas Vidas <hello@thomasvidas.com>
2021-11-04 18:21:56 -0400

    feat: bundle Capacitor and CapacitorCordova as a zip for IonicPortals Carthage support

---
+++ build-carthage-zip.sh
@@ -0,0 +1,14 @@
+# Capacitor build
+carthage build --no-skip-current --use-xcframeworks
+
+# CapacitorCordova build
+cd ios/CapacitorCordova
+xcodebuild archive -scheme Cordova -sdk iphonesimulator SKIP_INSTALL=NO -archivePath Build/iOS-Simulator
+xcodebuild archive -scheme Cordova -sdk iphoneos SKIP_INSTALL=NO -archivePath Build/iOS 
+xcodebuild -create-xcframework -framework Build/iOS.xcarchive/Products/Library/Frameworks/Cordova.framework -framework Build/iOS-Simulator.xcarchive/Products/Library/Frameworks/Cordova.framework -output CapacitorCordova.xcframework
+cd ../..
+
+# Zip
+mkdir Carthage/Build/CapacitorCordova.xcframework
+mv ios/CapacitorCordova/CapacitorCordova.xcframework/* Carthage/Build/CapacitorCordova.xcframework
+zip -r Capacitor.xcframework.zip Carthage/Build
