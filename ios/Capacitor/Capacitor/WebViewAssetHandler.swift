82929631092c40f387b187a7e8be0bccba87f94e
Steven Sherry <steven.sherry@ionic.io>
2022-05-18 12:04:37 -0500

    The undocumented Router protocol currently only allows for specifying relative paths. This change makes it possible to point to an entirely different base url (which is needed for portals for capacitor)

--- ios/Capacitor/Capacitor/WebViewAssetHandler.swift
+++ ios/Capacitor/Capacitor/WebViewAssetHandler.swift
@@ -3,8 +3,7 @@ import MobileCoreServices
 
 @objc(CAPWebViewAssetHandler)
 internal class WebViewAssetHandler: NSObject, WKURLSchemeHandler {
-    private let router: Router
-    private var basePath: String = ""
+    private var router: Router
     
     init(router: Router) {
         self.router = router
@@ -12,11 +11,11 @@ internal class WebViewAssetHandler: NSObject, WKURLSchemeHandler {
     }
 
     func setAssetPath(_ assetPath: String) {
-        self.basePath = assetPath
+        router.basePath = assetPath
     }
 
     func webView(_ webView: WKWebView, start urlSchemeTask: WKURLSchemeTask) {
-        var startPath = self.basePath
+        let startPath: String
         let url = urlSchemeTask.request.url!
         let stringToLoad = url.path
         
@@ -24,7 +23,7 @@ internal class WebViewAssetHandler: NSObject, WKURLSchemeHandler {
         if stringToLoad.starts(with: CapacitorBridge.fileStartIdentifier) {
             startPath = stringToLoad.replacingOccurrences(of: CapacitorBridge.fileStartIdentifier, with: "")
         } else {
-            startPath.append(resolvedRoute)
+            startPath = resolvedRoute
         }
         
         let localUrl = URL.init(string: url.absoluteString)!
