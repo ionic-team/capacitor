2e831337bc716bbc491dad114543654b905e4b16
jcesarmobile <jcesarmobile@gmail.com>
2022-06-23 15:50:40 +0200

    chore(ios): fmt (#5716)

--- ios/Capacitor/Capacitor/Router.swift
+++ ios/Capacitor/Capacitor/Router.swift
@@ -18,12 +18,12 @@ internal struct _Router: Router {
     var basePath: String = ""
     func route(for path: String) -> String {
         let pathUrl = URL(fileURLWithPath: path)
-       
+
         // If there's no path extension it also means the path is empty or a SPA route
         if pathUrl.pathExtension.isEmpty {
             return basePath + "/index.html"
         }
-        
+
         return basePath + path
     }
 }
