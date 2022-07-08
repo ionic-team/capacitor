d899b5ffc48bc257f16dc5d338ef0502be8b6296
Singh <Kundan.Singh@bakerhughes.com>
2022-07-08 16:59:42 +0530

    Fixed file select issue on Android 5.1.1 The file picker was not showing the filtered file.

--- android/capacitor/src/main/java/com/getcapacitor/BridgeWebChromeClient.java
+++ android/capacitor/src/main/java/com/getcapacitor/BridgeWebChromeClient.java
@@ -409,7 +409,9 @@ public class BridgeWebChromeClient extends WebChromeClient {
     }
 
     private void showFilePicker(final ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
-        Intent intent = fileChooserParams.createIntent();
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.putExtra("android.content.extra.SHOW_ADVANCED", true);
         if (fileChooserParams.getMode() == FileChooserParams.MODE_OPEN_MULTIPLE) {
             intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
         }
