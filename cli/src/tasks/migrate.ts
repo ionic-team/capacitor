25b82a84425bf09b2be45b213788b0e13982b9b3
jcesarmobile <jcesarmobile@gmail.com>
2022-07-28 17:30:28 +0200

    fix(cli): Correct Splash theme update (#5805)

--- cli/src/tasks/migrate.ts
+++ cli/src/tasks/migrate.ts
@@ -898,6 +898,9 @@ async function addNewSplashScreen(config: Config) {
 
   if (!stylesXml) return;
 
-  stylesXml = stylesXml.replace('AppTheme.NoActionBar', 'Theme.SplashScreen');
+  stylesXml = stylesXml.replace(
+    `parent="AppTheme.NoActionBar"`,
+    `parent="Theme.SplashScreen"`,
+  );
   writeFileSync(stylePath, stylesXml);
 }
