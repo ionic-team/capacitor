95b6bedacc587e19de584608a367dc728a604d6d
jcesarmobile <jcesarmobile@gmail.com>
2022-07-28 19:14:11 +0200

    chore(cli): Replace Dark theme with DayNight theme (#5807)

--- cli/src/tasks/migrate.ts
+++ cli/src/tasks/migrate.ts
@@ -295,9 +295,12 @@ export async function migrateCommand(config: Config): Promise<void> {
         rimraf.sync(join(config.android.appDirAbs, 'build'));
 
         // add new splashscreen
-        await runTask('Migrate to Android 12 Splashscreen.', () => {
-          return addNewSplashScreen(config);
-        });
+        await runTask(
+          'Migrate to Android 12 Splashscreen and apply DayNight theme.',
+          () => {
+            return addNewSplashScreen(config);
+          },
+        );
       }
 
       // Run Cap Sync
@@ -911,6 +914,12 @@ async function addNewSplashScreen(config: Config) {
   stylesXml = stylesXml.replace(
     `name="Theme.SplashScreenLaunch"`,
     `name="AppTheme.NoActionBarLaunch"`,
+  );
+
+  // Apply DayNight theme
+  stylesXml = stylesXml.replace(
+    `parent="Theme.AppCompat.NoActionBar"`,
+    `parent="Theme.AppCompat.DayNight.NoActionBar"`,
   );
 
   writeFileSync(stylePath, stylesXml);
