75f4dc0a3e38de3b711661f94c6ac82b516eac92
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2022-08-30 09:56:31 -0400

    feat(cli): add build command for android

---
+++ cli/src/tasks/build.ts
@@ -0,0 +1,62 @@
+import { buildAndroid } from '../android/build';
+import c from '../colors';
+import { selectPlatforms, promptForPlatform } from '../common';
+import type { Config } from '../definitions';
+import { fatal, isFatal } from '../errors';
+
+export interface BuildCommandOptions {
+  keystorepath: string;
+  keystorepass: string;
+  keystorealias: string;
+  keystorealiaspass: string;
+  androidreleasetype: string;
+}
+
+export async function buildCommand(
+  config: Config,
+  selectedPlatformName: string,
+  buildOptions: BuildCommandOptions,
+): Promise<void> {
+  const platforms = await selectPlatforms(config, selectedPlatformName);
+  let platformName: string;
+  if (platforms.length === 1) {
+    platformName = platforms[0];
+  } else {
+    platformName = await promptForPlatform(
+      platforms.filter(createBuildablePlatformFilter(config)),
+      `Please choose a platform to build for:`,
+    );
+  }
+
+  try {
+    await build(config, platformName, buildOptions);
+  } catch (e) {
+    if (!isFatal(e)) {
+      fatal((e as any).stack ?? e);
+    }
+    throw e;
+  }
+}
+
+export async function build(
+  config: Config,
+  platformName: string,
+  buildOptions: BuildCommandOptions,
+): Promise<void> {
+  if (platformName == config.ios.name) {
+    // await runIOS(config);
+  } else if (platformName === config.android.name) {
+    await buildAndroid(config, buildOptions);
+  } else if (platformName === config.web.name) {
+    throw `Platform "${platformName}" is not available in the build command.`;
+  } else {
+    throw `Platform "${platformName}" is not valid.`;
+  }
+}
+
+function createBuildablePlatformFilter(
+  config: Config,
+): (platform: string) => boolean {
+  return platform =>
+    platform === config.ios.name || platform === config.android.name;
+}
