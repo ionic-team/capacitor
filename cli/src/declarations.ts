85b1971419de1087f25ffdc97ccaf6f9527c9abe
jcesarmobile <jcesarmobile@gmail.com>
2022-09-15 17:45:39 +0200

    docs(cli): Improve information about minWebViewVersion and errorPath

--- cli/src/declarations.ts
+++ cli/src/declarations.ts
@@ -217,6 +217,10 @@ export interface CapacitorConfig {
      *
      * The minimum supported cannot be lower than version `55`, which is required for Capacitor.
      *
+     * If the device uses a lower WebView version, an error message will be shown on Logcat.
+     * If `server.errorPath` is configured, the WebView will redirect to that file, so can be
+     * used to show a custom error.
+     *
      * @since 4.0.0
      * @default 60
      */
@@ -457,6 +461,7 @@ export interface CapacitorConfig {
 
     /**
      * Specify path to a local html page to display in case of errors.
+     * On Android the html file won't have access to Capacitor plugins.
      *
      * @since 4.0.0
      * @default null
