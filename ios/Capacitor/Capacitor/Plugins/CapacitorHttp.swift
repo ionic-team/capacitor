dccb0a99850c7c878906156a509ecd675836ef1a
jcesarmobile <jcesarmobile@gmail.com>
2022-12-16 17:58:04 +0100

    fix(ios): encode whitespace in http plugin urls (#6169)

--- ios/Capacitor/Capacitor/Plugins/CapacitorHttp.swift
+++ ios/Capacitor/Capacitor/Plugins/CapacitorHttp.swift
@@ -3,10 +3,6 @@ import Foundation
 @objc(CAPHttpPlugin)
 public class CAPHttpPlugin: CAPPlugin {
     @objc func http(_ call: CAPPluginCall, _ httpMethod: String?) {
-        // Protect against bad values from JS before calling request
-        guard let url = call.getString("url") else { return call.reject("Must provide a URL"); }
-        guard var _ = URL(string: url) else { return call.reject("Invalid URL"); }
-
         do {
             try HttpRequestHandler.request(call, httpMethod, self.bridge?.config)
         } catch let error {
