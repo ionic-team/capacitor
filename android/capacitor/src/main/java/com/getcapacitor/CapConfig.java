26613d5a2c99bac6e11014b4f480b2547507e993
Carl Poole <carl@ionic.io>
2023-01-30 09:50:56 -0600

    feat(android): create configs from custom path and allow CapacitorWebView to read the set config

--- android/capacitor/src/main/java/com/getcapacitor/CapConfig.java
+++ android/capacitor/src/main/java/com/getcapacitor/CapConfig.java
@@ -78,7 +78,7 @@ public class CapConfig {
             this.configJSON = config;
         } else {
             // Load the capacitor.config.json
-            loadConfig(assetManager);
+            loadConfigFromAssets(assetManager, null);
         }
 
         deserializeConfig(null);
@@ -98,7 +98,47 @@ public class CapConfig {
             return config;
         }
 
-        config.loadConfig(context.getAssets());
+        config.loadConfigFromAssets(context.getAssets(), null);
+        config.deserializeConfig(context);
+        return config;
+    }
+
+    /**
+     * Constructs a Capacitor Configuration from config.json file within the app assets.
+     *
+     * @param context The context.
+     * @param path A path relative to the root assets directory.
+     * @return A loaded config file, if successful.
+     */
+    public static CapConfig loadFromAssets(Context context, String path) {
+        CapConfig config = new CapConfig();
+
+        if (context == null) {
+            Logger.error("Capacitor Config could not be created from file. Context must not be null.");
+            return config;
+        }
+
+        config.loadConfigFromAssets(context.getAssets(), path);
+        config.deserializeConfig(context);
+        return config;
+    }
+
+    /**
+     * Constructs a Capacitor Configuration from config.json file within the app file-space.
+     *
+     * @param context The context.
+     * @param path A path relative to the root assets directory.
+     * @return A loaded config file, if successful.
+     */
+    public static CapConfig loadFromFile(Context context, String path) {
+        CapConfig config = new CapConfig();
+
+        if (context == null) {
+            Logger.error("Capacitor Config could not be created from file. Context must not be null.");
+            return config;
+        }
+
+        config.loadConfigFromAssets(context.getAssets(), path);
         config.deserializeConfig(context);
         return config;
     }
@@ -142,13 +182,41 @@ public class CapConfig {
 
     /**
      * Loads a Capacitor Configuration JSON file into a Capacitor Configuration object.
+     * An optional path string can be provided to look for the config in a subdirectory path.
      */
-    private void loadConfig(AssetManager assetManager) {
+    private void loadConfigFromAssets(AssetManager assetManager, String path) {
+        if (path == null) {
+            path = "";
+        } else {
+            // Add slash at the end to form a proper file path if going deeper in assets dir
+            if (path.charAt(path.length()-1) != '/') {
+                path = path + "/";
+            }
+        }
+
         try {
-            String jsonString = readFile(assetManager, "capacitor.config.json");
+            String jsonString = readFile(assetManager, path + "capacitor.config.json");
             configJSON = new JSONObject(jsonString);
         } catch (IOException ex) {
             Logger.error("Unable to load capacitor.config.json. Run npx cap copy first", ex);
+        } catch (JSONException ex) {
+            Logger.error("Unable to parse capacitor.config.json. Make sure it's valid json", ex);
+        }
+    }
+
+    /**
+     * Loads a Capacitor Configuration JSON file into a Capacitor Configuration object.
+     * An optional path string can be provided to look for the config in a subdirectory path.
+     */
+    private void loadConfigFromFile(String path) {
+        if (path == null) {
+            path = "";
+        }
+
+        try {
+            //String jsonString = readFile(assetManager, path + "capacitor.config.json");
+            String jsonString = "";
+            configJSON = new JSONObject(jsonString);
         } catch (JSONException ex) {
             Logger.error("Unable to parse capacitor.config.json. Make sure it's valid json", ex);
         }
