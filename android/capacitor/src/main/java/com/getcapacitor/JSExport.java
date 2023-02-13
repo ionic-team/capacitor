42b4f0f416c8038ae368860007910bb09c8ec84e
Carl Poole <carl@ionic.io>
2023-02-13 11:22:27 -0600

    feat(android): add ability to create config from a custom file path (#6264)

--- android/capacitor/src/main/java/com/getcapacitor/JSExport.java
+++ android/capacitor/src/main/java/com/getcapacitor/JSExport.java
@@ -1,6 +1,6 @@
 package com.getcapacitor;
 
-import static com.getcapacitor.FileUtils.readFile;
+import static com.getcapacitor.FileUtils.readFileFromAssets;
 
 import android.content.Context;
 import android.text.TextUtils;
@@ -24,7 +24,7 @@ public class JSExport {
     public static String getCordovaJS(Context context) {
         String fileContent = "";
         try {
-            fileContent = readFile(context.getAssets(), "public/cordova.js");
+            fileContent = readFileFromAssets(context.getAssets(), "public/cordova.js");
         } catch (IOException ex) {
             Logger.error("Unable to read public/cordova.js file, Cordova plugins will not work");
         }
@@ -34,7 +34,7 @@ public class JSExport {
     public static String getCordovaPluginsFileJS(Context context) {
         String fileContent = "";
         try {
-            fileContent = readFile(context.getAssets(), "public/cordova_plugins.js");
+            fileContent = readFileFromAssets(context.getAssets(), "public/cordova_plugins.js");
         } catch (IOException ex) {
             Logger.error("Unable to read public/cordova_plugins.js file, Cordova plugins will not work");
         }
@@ -91,7 +91,7 @@ public class JSExport {
                     }
                 }
             } else {
-                return readFile(context.getAssets(), path);
+                return readFileFromAssets(context.getAssets(), path);
             }
         } catch (IOException ex) {
             Logger.warn("Unable to read file at path " + path);
