867a9cf0724e061be719c963f62c0adce60ac5cd
Dominik Bruhn <dominik@dbruhn.de>
2023-08-16 15:06:54 +0200

    Fix case of HTTP Response Headers in IOS
    The Apple Foundation HTTP Code returns the dictionary of Response
    Headers capitalized (i.e. "Content-Type") [1]. The expectation from
    Javascript side is that HTTP Response Headers when queried via the
    `getResponseHeader` method are matched case insensitive [2]. The
    current implementation of the Capacitor HTTP Plugin does not correctly
    bring this together. So calling `getResponseHeader("content-type")`
    fails currently (i.e. returns nothing) even if the `content-type` header
    is present, although the matching should be done case insensitive.
    
    This change does two things:
    - The HTTP Response Headers which are returned in a dictionary are
      transformed and the keys are lowercased. The value is preserved.
    - When querying the value of a header in the `getResponseHeader`
      function in the bridge code, the header is also lowercased.
    
    This should make the requesting of response haders case insensitive on
    IOS.
    
    [1]: https://developer.apple.com/documentation/foundation/httpurlresponse/1417930-allheaderfields
    [2]: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/getResponseHeader

--- ios/Capacitor/Capacitor/assets/native-bridge.js
+++ ios/Capacitor/Capacitor/assets/native-bridge.js
@@ -669,7 +669,9 @@ var nativeBridge = (function (exports) {
                             if (isRelativeURL(this._url)) {
                                 return win.CapacitorWebXMLHttpRequest.getResponseHeader.call(this, name);
                             }
-                            return this._headers[name];
+                            // The search for the name is case insenstive. The Swift Code makes sure that the keys
+                            // in the headers dictionary are already lowercased.
+                            return this._headers[name.toLowerCase()];
                         };
                         Object.setPrototypeOf(xhr, prototype);
                         return xhr;
