4b18fd58836c5a3974cc37700265689d3c4d8be4
Anders Hausding <ahausding@sensorstim.com>
2023-08-28 14:08:18 +0200

    fix(ios): add removeAllListeners selector

--- ios/Capacitor/Capacitor/CapacitorBridge.swift
+++ ios/Capacitor/Capacitor/CapacitorBridge.swift
@@ -449,7 +449,7 @@ internal class CapacitorBridge: NSObject, CAPBridgeProtocol {
         }
 
         let selector: Selector
-        if call.method == "addListener" || call.method == "removeListener" {
+        if call.method == "addListener" || call.method == "removeListener" || call.method == "removeAllListeners" {
             selector = NSSelectorFromString(call.method + ":")
         } else {
             guard let method = plugin.getMethod(named: call.method) else {
