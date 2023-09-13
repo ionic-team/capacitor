91c11d06f773c45a10f6f2d52f672ae6f189b162
Chace Daniels <chaced12@gmail.com>
2023-09-13 13:48:12 -0700

    fix(http): inherit object properties on window.XMLHttpRequest

--- android/capacitor/src/main/assets/native-bridge.js
+++ android/capacitor/src/main/assets/native-bridge.js
@@ -394,6 +394,7 @@ var nativeBridge = (function (exports) {
                 win.CapacitorWebXMLHttpRequest = {
                     abort: window.XMLHttpRequest.prototype.abort,
                     constructor: window.XMLHttpRequest.prototype.constructor,
+                    fullObject: window.XMLHttpRequest,
                     getAllResponseHeaders: window.XMLHttpRequest.prototype.getAllResponseHeaders,
                     getResponseHeader: window.XMLHttpRequest.prototype.getResponseHeader,
                     open: window.XMLHttpRequest.prototype.open,
@@ -674,6 +675,7 @@ var nativeBridge = (function (exports) {
                         Object.setPrototypeOf(xhr, prototype);
                         return xhr;
                     };
+                    Object.assign(window.XMLHttpRequest, win.CapacitorWebXMLHttpRequest.fullObject);
                 }
             }
             // patch window.console on iOS and store original console fns
