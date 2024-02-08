40128b6c63d7af4dd3016ae6ad3b5eecd546bce3
Joseph Pender <joey@ionic.io>
2024-02-08 16:46:20 -0600

    Extracting project template from package subfolder

--- cli/src/util/template.ts
+++ cli/src/util/template.ts
@@ -1,12 +1,25 @@
 import { mkdirp, cpSync, lstatSync } from '@ionic/utils-fs';
+import { join } from 'node:path';
 import tar from 'tar';
 
 export async function extractTemplate(src: string, dir: string): Promise<void> {
   await mkdirp(dir);
   if (!lstatSync(src).isDirectory()) {
-    await tar.extract({ file: src, cwd: dir });
+    await tar.extract({
+      file: src,
+      cwd: dir,
+      filter: (path, entry) => {
+        if (path.startsWith('./template/')) {
+          (entry.path as unknown as string) = (
+            entry.path as unknown as string
+          ).replace('./template/', './');
+          return true;
+        }
+        return false;
+      },
+    });
   } else {
-    cpSync(src, dir, {
+    cpSync(join(src, 'template'), dir, {
       recursive: true,
     });
   }
