b71635c32d1465d3434db3c94d0c52002a765016
jcesarmobile <jcesarmobile@gmail.com>
2024-02-29 15:58:09 +0100

    fix(android): let WebView handle errors (#7304)

--- android/capacitor/src/main/java/com/getcapacitor/WebViewLocalServer.java
+++ android/capacitor/src/main/java/com/getcapacitor/WebViewLocalServer.java
@@ -184,6 +184,7 @@ public class WebViewLocalServer {
                 return handleCapacitorHttpRequest(request);
             } catch (Exception e) {
                 Logger.error(e.getLocalizedMessage());
+                return null;
             }
         }
 
