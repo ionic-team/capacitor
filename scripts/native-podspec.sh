d04f5ad59ba5c4feafe2488b7d72764ed115f71d
jcesarmobile <jcesarmobile@gmail.com>
2024-03-12 17:46:45 +0100

    chore(ci): Re enable pod validation failures (#7331)

--- scripts/native-podspec.sh
+++ scripts/native-podspec.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
-
-export NATIVE_PUBLISH=true
+-set -eo pipefail
 
 case $1 in
      lint) 
-       pod spec lint ios/CapacitorCordova.podspec --allow-warnings
-       pod spec lint ios/Capacitor.podspec --allow-warnings;;
+       pod lib lint ios/CapacitorCordova.podspec --allow-warnings
+       pod lib lint ios/Capacitor.podspec --allow-warnings;;
 
      publish) 
+       export NATIVE_PUBLISH=true
        pod trunk push ios/CapacitorCordova.podspec --allow-warnings
        pod trunk push ios/Capacitor.podspec --allow-warnings;;
 
