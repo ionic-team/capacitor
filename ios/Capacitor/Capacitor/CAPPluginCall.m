bf6ef8e1472a789d3216686ed5141f84cc8b95ff
Steven Sherry <steven.sherry@ionic.io>
2024-09-27 12:41:57 -0500

    feat(core): cherrypick: expose `methodName` via `CAPPluginCall` (#7641) (#7684)
    Co-authored-by: Robin Genz <r.genz@snapaddy.com>

--- ios/Capacitor/Capacitor/CAPPluginCall.m
+++ ios/Capacitor/Capacitor/CAPPluginCall.m
@@ -3,8 +3,18 @@
 
 @implementation CAPPluginCall
 
-- (instancetype)initWithCallbackId:(NSString *)callbackId options:(NSDictionary *)options success:(CAPPluginCallSuccessHandler) success error:(CAPPluginCallErrorHandler) error {
+- (instancetype)initWithCallbackId:(NSString *)callbackId options:(NSDictionary *)options success:(CAPPluginCallSuccessHandler) success error:(CAPPluginCallErrorHandler) error __deprecated {
   self.callbackId = callbackId;
+  self.methodName = @"";
+  self.options = options;
+  self.successHandler = success;
+  self.errorHandler = error;
+  return self;
+}
+
+- (instancetype)initWithCallbackId:(NSString *)callbackId methodName:(NSString *)methodName options:(NSDictionary *)options success:(CAPPluginCallSuccessHandler) success error:(CAPPluginCallErrorHandler) error {
+  self.callbackId = callbackId;
+  self.methodName = methodName;
   self.options = options;
   self.successHandler = success;
   self.errorHandler = error;
