04e9576370781cb8b0a36e6459b36498abcac4f4
Eric Horodyski <horodyski@ionic.io>
2024-10-16 14:19:02 -0400

    feat(cli): update messaging to mention the 'off' toggle to disable telemetry

--- cli/src/telemetry.ts
+++ cli/src/telemetry.ts
@@ -15,7 +15,7 @@ const debug = Debug('capacitor:telemetry');
 const THANK_YOU =
   `\nThank you for helping improve Capacitor by sharing anonymous usage data! 💖` +
   `\nInformation about the data we collect is available on our website: ${c.strong('https://capacitorjs.com/telemetry')}` +
-  `\nYou can disable telemetry at any time by using the ${c.input('npx cap telemetry')} command.`;
+  `\nYou can disable telemetry at any time by using the ${c.input('npx cap telemetry off')} command.`;
 
 export interface CommandMetricData {
   app_id: string;
