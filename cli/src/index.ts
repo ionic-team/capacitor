23300df62ebaf9692a46ad5829581b71d4f9177a
Joseph Pender <joey.pender@outsystems.com>
2024-11-19 13:34:39 -0600

    fmt

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -160,7 +160,10 @@ export function runProgram(config: Config): void {
       ),
     )
     .addOption(
-      new Option('--xcode-provisioning-profile <xcodeProvisioningProfile>', 'A provisioning profile name or UUID for iOS builds'),
+      new Option(
+        '--xcode-provisioning-profile <xcodeProvisioningProfile>',
+        'A provisioning profile name or UUID for iOS builds',
+      ),
     )
     .action(
       wrapAction(
