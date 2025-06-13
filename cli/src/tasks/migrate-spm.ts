9c292616a40d08090ea8c4978986abf131325cac
Mark Anderson <mark.anderson@outsystems.com>
2025-06-13 15:26:52 -0400

    Add Update to migrate-spm

--- cli/src/tasks/migrate-spm.ts
+++ cli/src/tasks/migrate-spm.ts
@@ -8,6 +8,7 @@ import {
   runCocoapodsDeintegrate,
   addInfoPlistDebugIfNeeded,
 } from '../util/spm';
+import { update } from './update';
 
 export async function migrateToSPM(config: Config): Promise<void> {
   if ((await checkPackageManager(config)) == 'SPM') {
@@ -18,6 +19,7 @@ export async function migrateToSPM(config: Config): Promise<void> {
   await runCocoapodsDeintegrate(config);
   await removeCocoapodsFiles(config);
   await addInfoPlistDebugIfNeeded(config);
+  await update(config, 'ios', true)
 
   logger.info(
     'To complete migration follow the manual steps at https://capacitorjs.com/docs/ios/spm#using-our-migration-tool',
