6ddb7b929ab30b121b6819acd61e320d1c82fa91
Joseph Pender <joey.pender@outsystems.com>
2025-09-17 15:56:34 -0500

    fmt

--- ios/Capacitor/Capacitor/Plugins/SystemBars.swift
+++ ios/Capacitor/Capacitor/Plugins/SystemBars.swift
@@ -6,18 +6,17 @@ public class CAPSystemBarsPlugin: CAPPlugin, CAPBridgedPlugin {
     public let jsName = "SystemBars"
     public let pluginMethods: [CAPPluginMethod] = [
         CAPPluginMethod(name: "setStyle", returnType: CAPPluginReturnNone),
-        CAPPluginMethod(name: "setHidden", returnType: CAPPluginReturnNone),
+        CAPPluginMethod(name: "setHidden", returnType: CAPPluginReturnNone)
     ]
-    
+
     enum Style: String {
         case dark = "DARK"
         case light = "LIGHT"
     }
-    
-    
+
     @objc func setStyle(_ call: CAPPluginCall) {
         var newStyle: UIStatusBarStyle = .default
-        
+
         if let style = Style(rawValue: call.getString("style") ?? "") {
             switch style {
             case .dark:
@@ -26,10 +25,10 @@ public class CAPSystemBarsPlugin: CAPPlugin, CAPBridgedPlugin {
                 newStyle = .lightContent
             }
         }
-        
+
         bridge?.statusBarStyle = newStyle
     }
-    
+
     @objc func setHidden(_ call: CAPPluginCall) {
         let hidden = call.getBool("hidden") ?? false
         bridge?.statusBarVisible = !hidden
