e6bd10b8136c8147ca131cabf56d15f5527d88c4
Joseph Pender <joey.pender@outsystems.com>
2025-10-22 12:53:04 -0500

    Adding support for configurations in iOS system bars

--- ios/Capacitor/Capacitor/Plugins/SystemBars.swift
+++ ios/Capacitor/Capacitor/Plugins/SystemBars.swift
@@ -12,25 +12,45 @@ public class CAPSystemBarsPlugin: CAPPlugin, CAPBridgedPlugin {
     enum Style: String {
         case dark = "DARK"
         case light = "LIGHT"
+        case defaultStyle = "DEFAULT"
+    }
+    
+    @objc override public func load() {
+        let style = getConfig().getString("style", Style.defaultStyle.rawValue) ?? Style.defaultStyle.rawValue
+        let hidden = getConfig().getBoolean("hidden", false)
     }
 
     @objc func setStyle(_ call: CAPPluginCall) {
+        setStyle(style: call.getString("style") ?? Style.defaultStyle.rawValue)
+        
+        call.resolve()
+    }
+
+    @objc func setHidden(_ call: CAPPluginCall) {
+        let hidden = call.getBool("hidden") ?? false
+        setHidden(hidden: hidden)
+        
+        call.resolve()
+    }
+    
+    func setStyle(style: String) {
         var newStyle: UIStatusBarStyle = .default
 
-        if let style = Style(rawValue: call.getString("style") ?? "") {
+        if let style = Style(rawValue: style) {
             switch style {
             case .dark:
                 newStyle = .lightContent
             case .light:
                 newStyle = .darkContent
+            case .defaultStyle:
+                newStyle = .default
             }
         }
-
+        
         bridge?.statusBarStyle = newStyle
     }
-
-    @objc func setHidden(_ call: CAPPluginCall) {
-        let hidden = call.getBool("hidden") ?? false
+    
+    func setHidden(hidden: Bool) {
         bridge?.statusBarVisible = !hidden
     }
 }
