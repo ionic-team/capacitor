b6e2175e85d3ad0d1799d4a520f7fd0de50e5875
Mark Anderson <mark.anderson@outsystems.com>
2026-02-19 10:30:42 -0500

    style: npm run fmt

--- release.config.mjs
+++ release.config.mjs
@@ -2,23 +2,21 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-  branches: [ '+([0-9])?(.{+([0-9]),x}).x',
-              'main',
-              { name: "next", channel: "next", prerelease: "dev"}, 
-            ],
-  tagFormat: "${version}",
+  branches: ['+([0-9])?(.{+([0-9]),x}).x', 'main', { name: 'next', channel: 'next', prerelease: 'dev' }],
+  tagFormat: '${version}',
   plugins: [
-    "@semantic-release/github",
-    [ "@semantic-release/commit-analyzer",
-      { 
-        "preset": "conventionalcommits",
+    '@semantic-release/github',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
       },
     ],
     [
-      "@semantic-release/release-notes-generator",
+      '@semantic-release/release-notes-generator',
       {
-        "preset": "conventionalcommits",
-      }
-    ]
+        preset: 'conventionalcommits',
+      },
+    ],
   ],
 };
