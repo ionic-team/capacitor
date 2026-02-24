2540b5ef1047a916843a067cd247b8411bfccfba
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-02-24 12:23:17 +0000

    docs(cli): Make parameter description less ambiguous
    Enabling HTTPS could mislead to indicate that the cli would handle certificate management, which it won't.

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -253,7 +253,7 @@ export function runProgram(config: Config): void {
     .option('--host <host>', 'Host used for live reload')
     .option('--port <port>', 'Port used for live reload')
     .option('--configuration <name>', 'Configuration name of the iOS Scheme')
-    .option('--https', 'Enable HTTPS for live reload server')
+    .option('--https', 'Use https:// instead of http:// for the live reload server')
     .action(
       wrapAction(
         telemetryAction(
