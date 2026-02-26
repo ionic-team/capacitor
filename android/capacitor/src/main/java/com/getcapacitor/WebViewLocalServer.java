ef9432c543f051bae877fb209d503ad9d0c5feb6
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-02-26 12:47:18 +0000

    fix(android): Safely parse range integer

--- android/capacitor/src/main/java/com/getcapacitor/WebViewLocalServer.java
+++ android/capacitor/src/main/java/com/getcapacitor/WebViewLocalServer.java
@@ -337,10 +337,13 @@ public class WebViewLocalServer {
                 String[] parts = rangeString.split("=");
                 String[] streamParts = parts[1].split("-");
                 String fromRange = streamParts[0];
-                int bytesToSkip = Integer.parseInt(fromRange);
-                if (bytesToSkip > 0) {
-                    responseStream.skip(bytesToSkip);
-                }
+                int bytesToSkip;
+                try {
+                    bytesToSkip = Integer.parseInt(fromRange);
+                    if (bytesToSkip > 0) {
+                        responseStream.skip(bytesToSkip);
+                    }
+                } catch (NumberFormatException ignored) {}
                 int range = totalRange - 1;
                 if (streamParts.length > 1) {
                     range = Integer.parseInt(streamParts[1]);
