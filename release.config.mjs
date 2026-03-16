ed0bdc34c52b0be1931c6dfbb8222f197b888d60
Mark Anderson <mark.anderson@outsystems.com>
2026-03-16 13:21:42 -0400

    do not push

--- release.config.mjs
+++ release.config.mjs
@@ -2,7 +2,7 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-  branches: ['+([0-9])?(.{+([0-9]),x}).x', 'main', { name: 'next', channel: 'next', prerelease: 'dev' }],
+  branches: ['+([0-9])?(.{+([0-9]),x}).x', 'main', { name: 'semantic', channel: 'next', prerelease: 'dev' }],
   tagFormat: '${version}',
   plugins: [
     '@semantic-release/github',
