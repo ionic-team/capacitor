22545da9871e38082b790cfd42e1774b0ed20085
jcesarmobile <jcesarmobile@gmail.com>
2026-03-16 18:04:08 +0100

    fix(http): handle URL objects on fetch (#8386) (#8387)

--- android/capacitor/src/main/assets/native-bridge.js
+++ android/capacitor/src/main/assets/native-bridge.js
@@ -499,6 +499,10 @@ var nativeBridge = (function (exports) {
                             if (typeof resource === 'string') {
                                 return await win.CapacitorWebFetch(createProxyUrl(resource, win), options);
                             }
+                            else if (resource instanceof URL) {
+                                const modifiedURL = new URL(createProxyUrl(resource.toString(), win));
+                                return await win.CapacitorWebFetch(modifiedURL, options);
+                            }
                             else if (resource instanceof Request) {
                                 const modifiedRequest = new Request(createProxyUrl(resource.url, win), resource);
                                 return await win.CapacitorWebFetch(modifiedRequest, options);
