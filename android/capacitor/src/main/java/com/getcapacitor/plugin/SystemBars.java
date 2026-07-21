e3e5a507ec7285b307752de0f3077c7e3b259d14
tafelnl <35837839+tafelnl@users.noreply.github.com>
2026-07-21 14:50:28 +0200

    style: run `npm run fmt`

--- android/capacitor/src/main/java/com/getcapacitor/plugin/SystemBars.java
+++ android/capacitor/src/main/java/com/getcapacitor/plugin/SystemBars.java
@@ -64,13 +64,27 @@ public class SystemBars extends Plugin {
     private WebViewListener webViewListener;
 
     private void warnAboutUnsupportedConfigurationValues() {
-        String systemBarsInsetsHandling = bridge.getConfig().getPluginConfiguration("SystemBars").getConfigJSON().optString("insetsHandling");
-        boolean keyboardResizeOnFullScreen = bridge.getConfig().getPluginConfiguration("Keyboard").getConfigJSON().optBoolean("resizeOnFullScreen", false);
+        String systemBarsInsetsHandling = bridge
+            .getConfig()
+            .getPluginConfiguration("SystemBars")
+            .getConfigJSON()
+            .optString("insetsHandling");
+        boolean keyboardResizeOnFullScreen = bridge
+            .getConfig()
+            .getPluginConfiguration("Keyboard")
+            .getConfigJSON()
+            .optBoolean("resizeOnFullScreen", false);
         if (!systemBarsInsetsHandling.equals("disable") && keyboardResizeOnFullScreen) {
-            Logger.warn("SystemBars", "You should omit `Keyboard.resizeOnFullScreen` in your `capacitor.config.json`. Other values can lead to unexpected behavior.");
+            Logger.warn(
+                "SystemBars",
+                "You should omit `Keyboard.resizeOnFullScreen` in your `capacitor.config.json`. Other values can lead to unexpected behavior."
+            );
         }
         if (isSafeAreaPluginPresent()) {
-            Logger.warn("SystemBars", "You should uninstall `@capacitor-community/safe-area`. Having this library installed can lead to unexpected behavior.");
+            Logger.warn(
+                "SystemBars",
+                "You should uninstall `@capacitor-community/safe-area`. Having this library installed can lead to unexpected behavior."
+            );
         }
     }
 
@@ -124,7 +138,11 @@ public class SystemBars extends Plugin {
         boolean hidden = getConfig().getBoolean("hidden", false);
 
         String configuredInsetsHandling = getConfig().getString("insetsHandling", INSETS_HANDLING_NATIVE);
-        if (INSETS_HANDLING_CSS.equals(configuredInsetsHandling) || INSETS_HANDLING_DISABLE.equals(configuredInsetsHandling) || INSETS_HANDLING_NATIVE.equals(configuredInsetsHandling)) {
+        if (
+            INSETS_HANDLING_CSS.equals(configuredInsetsHandling) ||
+            INSETS_HANDLING_DISABLE.equals(configuredInsetsHandling) ||
+            INSETS_HANDLING_NATIVE.equals(configuredInsetsHandling)
+        ) {
             insetsHandling = configuredInsetsHandling;
         } else {
             Logger.warn(
