5832336d8a9d57d250a437718ec92102c361d717
Joseph Pender <joey.pender@outsystems.com>
2026-07-27 12:54:42 -0500

    fmt

--- ios-spm-template/App/App/SceneDelegate.swift
+++ ios-spm-template/App/App/SceneDelegate.swift
@@ -5,13 +5,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-      guard let windowScene = scene as? UIWindowScene else { return }
+        guard let windowScene = scene as? UIWindowScene else { return }
 
-      window = UIWindow(windowScene: windowScene)
-      window?.rootViewController = CAPBridgeViewController()
-      window?.makeKeyAndVisible()
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = CAPBridgeViewController()
+        window?.makeKeyAndVisible()
 
-      SceneDelegateProxy.shared.scene(scene, willConnectTo: session, options: connectionOptions)
+        SceneDelegateProxy.shared.scene(scene, willConnectTo: session, options: connectionOptions)
     }
 
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
