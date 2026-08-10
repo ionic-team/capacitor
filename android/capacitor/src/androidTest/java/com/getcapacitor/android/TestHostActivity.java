a4e946bb6d7b6ca0e0e231e653c4245ff0ab16de
Chace Daniels <chace.daniels@outsystems.com>
2026-08-10 17:44:35 -0500

    fix: block navigation to the internal HTTP proxy path

---
+++ android/capacitor/src/androidTest/java/com/getcapacitor/android/TestHostActivity.java
@@ -0,0 +1,6 @@
+package com.getcapacitor.android;
+
+import com.getcapacitor.BridgeActivity;
+
+/** Minimal Capacitor host used to obtain a fully initialized Bridge in instrumented tests. */
+public class TestHostActivity extends BridgeActivity {}
