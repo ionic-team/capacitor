d2ad11714f7432aab714d86c4c47bae7e8d46b5d
Rui Moreira Mendes <rui.moreira.mendes@outsystems.com>
2026-08-11 13:12:36 +0100

    refactor(android)remove deprecated APIs orphaned by @NativePlugin drop

--- android/capacitor/src/main/java/com/getcapacitor/Bridge.java
+++ android/capacitor/src/main/java/com/getcapacitor/Bridge.java
@@ -1006,16 +1006,6 @@ public class Bridge {
         }
     }
 
-    @Deprecated
-    @SuppressWarnings("deprecation")
-    public void startActivityForPluginWithResult(PluginCall call, Intent intent, int requestCode) {
-        Logger.debug("Starting activity for result");
-
-        pluginCallForLastActivity = call;
-
-        getActivity().startActivityForResult(intent, requestCode);
-    }
-
     /**
      * Check for Cordova plugins that may have registered to handle a permission
      * request, and handle them if so. If not handled, false is returned.
@@ -1178,21 +1168,7 @@ public class Bridge {
             return false;
         }
 
-        // deprecated, to be removed
-        PluginCall lastCall = plugin.getInstance().getSavedCall();
-
-        // If we don't have a saved last call (because our app was killed and restarted, for example),
-        // Then we should see if we have any saved plugin call information and generate a new,
-        // "dangling" plugin call (a plugin call that doesn't have a corresponding web callback)
-        // and then send that to the plugin
-        if (lastCall == null && pluginCallForLastActivity != null) {
-            plugin.getInstance().saveCall(pluginCallForLastActivity);
-        }
-
         plugin.getInstance().handleOnActivityResult(requestCode, resultCode, data);
-
-        // Clear the plugin call we may have re-hydrated on app launch
-        pluginCallForLastActivity = null;
 
         return true;
     }
