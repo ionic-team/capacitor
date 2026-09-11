fed411a1f7b2aa03a2a870d553c98781f69cdb54
Joseph Pender <joey@prodcode.net>
2026-09-11 10:32:39 -0500

    fmt

--- android-template/app/src/main/java/com/getcapacitor/myapp/MainActivity.java
+++ android-template/app/src/main/java/com/getcapacitor/myapp/MainActivity.java
@@ -1,15 +1,14 @@
 package com.getcapacitor.myapp;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
-
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    EdgeToEdge.enable(this);
-  }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+    }
 }
